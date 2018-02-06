ed4d9b9eb1c36c605dcc1950cd79d0ba100be1c2
mariano78 <mariano78@interia.pl>
2018-02-06 10:20:21 +0100

    th

--- modules/thingspeak/thing_send.php
+++ modules/thingspeak/thing_send.php
@@ -6,23 +6,22 @@ $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 // main loop
 $rows = $db->query("SELECT * FROM thingspeak WHERE active='on' ");
 $row = $rows->fetchAll();
-	foreach ($row as $a => $item) {
+	foreach ($row as $a) {
 			
 			$url = 'http://api.thingspeak.com/update';
 			$ThingSpeakApiKey = $a['apikey'];
 			
+			$rom1 = $a['f1'];
+			$field1 = $db->query("SELECT tmp FROM sensors WHERE rom='$rom1' ");
+			$rom2 = $a['f2'];
+			$rom3 = $a['f3'];
+			$rom4 = $a['f4'];
+			$rom5 = $a['f5'];
+			$rom6 = $a['f6'];
+			$rom7 = $a['f7'];
+			$rom8 = $a['f8'];		
 			
-			
-			$item = $row[$a]['f1'];
-			//$rom2 = $a['f2'];
-			//$rom3 = $a['f3'];
-			//$rom4 = $a['f4'];
-			//$rom5 = $a['f5'];
-			//$rom6 = $a['f6'];
-			//$rom7 = $a['f7'];
-			//$rom8 = $a['f8'];		
-			
-			$data = 'key=' . $ThingSpeakApiKey . '&field1=' . $item ;
+			$data = 'key=' . $ThingSpeakApiKey . '&field1=' . $field1 . '&field2=' . $field2 .'&field3=' . $field3 . '&field4=' . $field4 . '&field5=' . $field5 . '&field6=' . $field6 . '&field7=' . $field7 . '&field8=' . $field8;
 			 
 			$ch = curl_init($url);
 			curl_setopt( $ch, CURLOPT_POST, 1);
