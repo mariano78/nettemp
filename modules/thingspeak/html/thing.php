772323b30649d57fc460ea383cf8f15c1aaa6f7e
mariano78 <mariano78@interia.pl>
2018-02-06 18:20:35 +0100

    th

--- modules/thingspeak/html/thing.php
+++ modules/thingspeak/html/thing.php
@@ -144,7 +144,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f2" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f2'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
@@ -154,7 +154,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f3" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f3'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
@@ -164,7 +164,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f4" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f4'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
@@ -174,7 +174,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f5" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f5'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
@@ -184,7 +184,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f6" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f6'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
@@ -194,7 +194,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f7" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f7'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
@@ -204,7 +204,7 @@ foreach ($row as $a) {
 	
 	<td class="col-md-1">
 		<select name="f8" class="form-control input-sm">
-		<option>---</option>
+		<option value="off">---</option>
 		<?php 
 			foreach ($result as $select) { ?>
 			<option value="<?php echo $select['rom']; ?>" <?php echo $select['rom']==$a['f8'] ? 'selected="selected"' : ''; ?> ><?php echo $select['name']." ".$select['tmp'] ?></option>
--- modules/thingspeak/thing_send.php
+++ modules/thingspeak/thing_send.php
@@ -4,24 +4,22 @@ $ROOT=dirname(dirname(dirname(__FILE__)));
 $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 
 // main loop
-//$rows = $db->query("SELECT * FROM thingspeak WHERE active='on' ");
-$rows = $db->query("
-
-SELECT t1.*,t2_1.tmp as tmp1,t2_2.tmp as tmp2,t2_3.tmp as tmp3,t2_4.tmp as tmp4,t2_5.tmp as tmp5,t2_6.tmp as tmp6,t2_7.tmp as tmp7,t2_8.tmp as tmp8 FROM thingspeak t1 LEFT JOIN sensors t2_1 ON t2_1.rom = t1.f1 LEFT JOIN sensors t2_2 ON t2_2.rom = t1.f2 LEFT JOIN sensors t2_3 ON t2_3.rom = t1.f3 LEFT JOIN sensors t2_4 ON t2_4.rom = t1.f4 LEFT JOIN sensors t2_5 ON t2_5.rom = t1.f5 LEFT JOIN sensors t2_6 ON t2_6.rom = t1.f6 LEFT JOIN sensors t2_7 ON t2_7.rom = t1.f7 LEFT JOIN sensors t2_8 ON t2_8.rom = t1.f8 WHERE active='on' ");
+$rows = $db->query(" SELECT t1.*,t2_1.tmp as tmp1,t2_2.tmp as tmp2,t2_3.tmp as tmp3,t2_4.tmp as tmp4,t2_5.tmp as tmp5,t2_6.tmp as tmp6,t2_7.tmp as tmp7,t2_8.tmp as tmp8 FROM thingspeak t1 LEFT JOIN sensors t2_1 ON t2_1.rom = t1.f1 LEFT JOIN sensors t2_2 ON t2_2.rom = t1.f2 LEFT JOIN sensors t2_3 ON t2_3.rom = t1.f3 LEFT JOIN sensors t2_4 ON t2_4.rom = t1.f4 LEFT JOIN sensors t2_5 ON t2_5.rom = t1.f5 LEFT JOIN sensors t2_6 ON t2_6.rom = t1.f6 LEFT JOIN sensors t2_7 ON t2_7.rom = t1.f7 LEFT JOIN sensors t2_8 ON t2_8.rom = t1.f8 WHERE active='on' ");
 $row = $rows->fetchAll();
 	foreach ($row as $a) {
 			
 			$url = 'http://api.thingspeak.com/update';
 			$ThingSpeakApiKey = $a['apikey'];
 			
-			$field1 = $a['tmp1'];
-			$field2 = $a['tmp2'];
-			$field3 = $a['tmp3'];
-			$field4 = $a['tmp4'];
-			$field5 = $a['tmp5'];
-			$field6 = $a['tmp6'];
-			$field7 = $a['tmp7'];
-			$field8 = $a['tmp8'];			
+			if ($a['tmp1'] == 'off'){$field1 = '';} else {$field1 = $a['tmp1'];}
+			if ($a['tmp2'] == 'off'){$field2 = '';} else {$field1 = $a['tmp2'];}
+			if ($a['tmp3'] == 'off'){$field3 = '';} else {$field1 = $a['tmp3'];}
+			if ($a['tmp4'] == 'off'){$field4 = '';} else {$field1 = $a['tmp4'];}
+			if ($a['tmp5'] == 'off'){$field5 = '';} else {$field1 = $a['tmp5'];}
+			if ($a['tmp6'] == 'off'){$field6 = '';} else {$field1 = $a['tmp6'];}
+			if ($a['tmp7'] == 'off'){$field7 = '';} else {$field1 = $a['tmp7'];}
+			if ($a['tmp8'] == 'off'){$field8 = '';} else {$field1 = $a['tmp8'];}
+					
 			$data = 'key=' . $ThingSpeakApiKey . '&field1=' . $field1 . '&field2=' . $field2 .'&field3=' . $field3 . '&field4=' . $field4 . '&field5=' . $field5 . '&field6=' . $field6 . '&field7=' . $field7 . '&field8=' . $field8;
 			 
 			$ch = curl_init($url);
