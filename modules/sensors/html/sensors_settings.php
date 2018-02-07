7c092c328b2cd3af6b00bdf1180870c7e8c3d29f
mariano78 <mariano78@interia.pl>
2018-02-07 10:49:13 +0100

    se

--- modules/sensors/html/sensors_settings.php
+++ modules/sensors/html/sensors_settings.php
@@ -450,19 +450,66 @@ $row = $rows->fetchAll();
 <td></td>
 <td></td>
 <td></td>
-<td><form action="" method="post" style="display:inline!important;">
+<td>
+	<form action="" method="post" style="display:inline!important;">
 		<input type="hidden" name="add_all" value="logon" />
 		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span> </button>
     </form>
     <form action="" method="post" style="display:inline!important;">
 		<input type="hidden" name="del_all" value="logon" />
 		<button class="btn btn-xs btn-info "><span class="glyphicon glyphicon-minus"></span> </button>
-    </form></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
+    </form>
+</td>
+<td>
+	<form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="add_all" value="charts" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span> </button>
+    </form>
+    <form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="del_all" value="charts" />
+		<button class="btn btn-xs btn-info "><span class="glyphicon glyphicon-minus"></span> </button>
+	</form>
+</td>
+<td>
+	<form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="add_all" value="remote" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span> </button>
+    </form>
+    <form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="del_all" value="remote" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-minus"></span> </button>
+    </form>
+</td>
+<td>
+	<form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="add_all" value="minmax" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span> </button>
+    </form>
+    <form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="del_all" value="minmax" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-minus"></span> </button>
+    </form>
+</td>
+<td>
+	<form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="add_all" value="lcd" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span> </button>
+    </form>
+    <form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="del_all" value="lcd" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-minus"></span> </button>
+    </form>
+</td>
+<td>
+	<form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="add_all" value="jg" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span> </button>
+    </form>
+    <form action="" method="post" style="display:inline!important;">
+		<input type="hidden" name="del_all" value="jg" />
+		<button class="btn btn-xs btn-info"><span class="glyphicon glyphicon-minus"></span> </button>
+    </form>
+</td>
 </tr>
 </thead>
 
