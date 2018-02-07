0ce53dd231a5d387cc9ef2aa8b8c1c33ae6c6cdc
mariano78 <mariano78@interia.pl>
2018-02-07 14:36:08 +0100

    th

--- modules/tools/html/log/log_thingspeak.php
+++ modules/tools/html/log/log_thingspeak.php
@@ -13,7 +13,7 @@ $th_log_del = isset($_POST['th_log_del']) ? $_POST['th_log_del'] : '';
 <div class="panel-heading">Thing Speak</div>
 <div class="panel-body">
 
-<form action="" method="post">
+<form action="index.php?id=tools&type=log&log=thingspeak" method="post">
     <input type="submit" name="th_log_del" value="Clear" class="btn btn-xs btn-danger" />
 </form>
 <br />
