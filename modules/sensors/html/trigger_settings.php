2fe5542a1c601412d5be485061c8921d8c473e9c
mariano78 <mariano78@interia.pl>
2018-02-19 14:51:36 +0100

    tr

--- modules/sensors/html/trigger_settings.php
+++ modules/sensors/html/trigger_settings.php
@@ -1,5 +1,5 @@
 <?php
-$rows_trig = $db->query("SELECT name, trigzero, trigone FROM sensors WHERE type='volt' ");
+$rows_trig = $db->query("SELECT rom, name, trigzero, trigone FROM sensors WHERE type='volt' ");
 $rowtr = $rows_trig->fetchAll();
 ?>
 
@@ -14,7 +14,17 @@ $rowtr = $rows_trig->fetchAll();
 foreach($rowtr as $tr) { ?>
 	
 <tr>
-<td><?php echo $tr["name"]; ?> </td>
+<td><span class="label label-default"><?php echo str_replace("_", " ", $tr['name']) ?></span></td>
+<td>
+<form action="" method="post" style="display:inline!important;"> 
+		<input type="hidden" name="tmp_id" value="<?php echo $tr['rom']; ?>" />
+		<input type="text" name="trigzero" size="10" value="<?php echo $tr['trigerzero']; ?>" />
+		<input type="text" name="trigone" size="10" value="<?php echo $a['trigerone']; ?>" />
+		<input type="hidden" name="trigupdate" value="trigupdate" />
+		<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
+    </form>
+</td>
+
 <td><?php echo $tr["trigzero"]; ?> </td>
 <td><?php echo $tr["trigone"]; ?> </td>
 </tr>
