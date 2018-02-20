8b2b2c7f12c4adbd48d018d814841e7e143d5d96
mariano78 <mariano78@interia.pl>
2018-02-20 08:00:48 +0100

    tr

--- modules/sensors/html/trigger_settings.php
+++ modules/sensors/html/trigger_settings.php
@@ -45,7 +45,7 @@ foreach($rowtr as $tr) { ?>
 		<select name="zeroclr" class="form-control input-sm">
 		
 		<?php foreach ($labels as $color) { ?>
-		<option class="<?php echo $color; ?>" value="<?php echo $color; ?>"<?php echo $tr['trigzeroclr'] == $color ? 'selected="selected"' : ''; ?>><span class="<?php echo $color; ?>"><?php echo $color; ?></span></option>
+			<option class="<?php echo $color; ?>" value="<?php echo $color; ?>"<?php echo $tr['trigzeroclr'] == $color ? 'selected="selected"' : ''; ?>><?php echo $color; ?></option>
 		<?php } ?>
 			
 		</select>
@@ -56,7 +56,7 @@ foreach($rowtr as $tr) { ?>
 		<label>Color:</label>
 		<select name="oneclr" class="form-control input-sm">
 		<?php foreach ($labels as $color) { ?>
-			<option value="<?php echo $color; ?>"<?php echo $tr['trigoneclr'] == $color ? 'selected="selected"' : ''; ?>><?php echo $color; ?></option>
+			<option class="<?php echo $color; ?>" value="<?php echo $color; ?>"<?php echo $tr['trigoneclr'] == $color ? 'selected="selected"' : ''; ?>><?php echo $color; ?></option>
 		<?php } ?>
 		</select>
 		<input type="hidden" name="trigupdate" value="trigupdate" />
