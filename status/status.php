0f22c3968afacfab52e1c5c4f9c8c8dbdc2d8903
mariano78 <mariano78@interia.pl>
2018-03-01 18:27:45 +0100

    q

--- status/status.php
+++ status/status.php
@@ -79,7 +79,7 @@ Go to device scan!
 	foreach($owresult as $owg) {
 		$owb = $owg['body'];
 		$own = $owg['name'];
-		include('tmp/ownwidget$owb.php');
+		include('tmp/ownwidget${owb}.php');
 	}
     include('status/minmax_status.php'); 
     include('status/counters_status.php');
