23b600cebfc4bb909072f232e925ef4de6ccf86c
mariano78 <mariano78@interia.pl>
2018-03-05 09:41:35 +0100

    ow

--- modules/settings/ownwidget_edit.php
+++ modules/settings/ownwidget_edit.php
@@ -42,7 +42,7 @@ $addow = isset($_POST['addow']) ? $_POST['addow'] : '';
 if(!empty($addow) && ($addow == "addow")) { 
 	$ownr=substr(rand(), 0, 4);
 	$db = new PDO('sqlite:dbf/nettemp.db');
-	$db->exec("INSERT INTO ownwidget ('name', 'body', 'onoff', 'iflogon', ,refresh,) VALUES ('My_widget','$ownr', 'on', 'off')");
+	$db->exec("INSERT INTO ownwidget ('name', 'body', 'onoff', 'iflogon', 'refresh') VALUES ('My_widget','$ownr', 'on', 'off')");
 	header("location: " . $_SERVER['REQUEST_URI']);
 	exit();	
 } 
