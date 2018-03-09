19ffc39ba64dcd86a72e891875317782959a7d51
mariano78 <mariano78@interia.pl>
2018-03-09 13:40:42 +0100

    v

--- modules/sensors/virtual.php
+++ modules/sensors/virtual.php
@@ -11,52 +11,61 @@ try {
     echo $date." Could not connect to the database.\n";
     exit;
 }
+
+
 try {
-    $sth = $db->query("SELECT * FROM nt_settings");
+    $sth = $db->query("SELECT * FROM sensors WHERE device = 'virtual'");
 	$sth->execute();
 	$result = $sth->fetchAll();
-	foreach ($result as $a) {
-		if($a['option']=='client_ip') {
-			$client_ip=$a['value'];
-		}
-		if($a['option']=='client_key') {
-			$client_key=$a['value'];
-		}
-		if($a['option']=='client_on') {
-			$client_on=$a['value'];
-		}
-		if($a['option']=='cauth_on') {
-			$cauth_on=$a['value'];
-		}
-		if($a['option']=='cauth_pass') {
-			$cauth_pass=$a['value'];
-		}
-}
-    
-    
-    
-    
-    if(!empty($client_ip)&&!empty($client_key)&&!empty($client_on)){
-		$query = $db->query("SELECT * FROM sensors WHERE remote='on'");
-		$result= $query->fetchAll();
-		foreach($result as $s) {
-			$rom=$s['rom'];
-			$rom2="remote_".$hostname."_".$rom;
-			$value=$s['tmp'];
-			$type=$s['type'];
-			$name=$s['name'];
-			$URL="http://".$client_ip."/receiver.php?key=".$client_key."&type=".$type."&rom=".$rom2."&value=".$value."&device=ip";
 	
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $URL);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERPWD, "admin:" . $cauth_pass);
-			$server_output = curl_exec ($ch);
-			curl_close ($ch);
-			echo $date." Name:".$name." Rom:".$rom." Value:".$value."\n";
-			echo $server_output;
+	include("$ROOT/receiver.php");
+	
+	foreach ($result as $a) {
+		
+		
+		if (substr($a['type'],0,3) == 'air'){
+			
+			$lati = $a['latitude'];
+			$long = $a['longitude'];
+			$api = $a['apikey'];
+			$localid = $a['id'];
+			$local_rom = $a['rom'];
+			$local_type = $a['type'];
+			$local_device = $a['device'];
+			
+			
+	$json = file_get_contents('https://airapi.airly.eu/v1/nearestSensor/measurements?latitude=$lati&longitude=$long&maxDistance=1000&apikey=$api');
+	$obj = json_decode($json,true);
+	
+	if ($local_type == "airquality") {
+		
+		$local_val = round($obj["airQualityIndex"])
+	}elseif ($local_type == "air_pm_25") {
+		$local_val = round($obj["pm25"])
+	}elseif ($local_type == "air_pm_10") {
+		$local_val = round($obj["pm10"])
+	}
+			
+		db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
+			
+			
 		}
-    }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+    
+    
+    
+    
+    
 
 
 } catch (Exception $e) {
