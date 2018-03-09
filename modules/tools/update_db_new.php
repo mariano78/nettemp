078252eaeb9f55614e9c597a94b420a15b1f66b4
mariano78 <mariano78@interia.pl>
2018-03-09 10:00:57 +0100

    v

--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -99,7 +99,7 @@ $updates['2018-03-05 09:29:00'][]="UPDATE ownwidget SET refresh='off'";
 //Virtual Sensors
 $updates['2018-03-09 09:29:46'][]="CREATE TABLE virtual (id INTEGER PRIMARY KEY, name TEXT , rom TEXT, type TEXT, device TEXT, lati TEXT, long TEXT, active TEXT)";
 $updates['2018-03-09 09:40:04'][]="INSERT INTO virtual  ('name', 'rom', 'type', 'device') VALUES ('Air_quality', 'Airly', 'airquality', 'virtual')";
-$updates['2018-03-09 09:40:04'][]="INSERT INTO virtual  ('name', 'rom', 'type', 'device') VALUES ('Air_quality_PM2.5', 'Airly', 'airqualitypm2.5', 'virtual')";
-$updates['2018-03-09 09:40:04'][]="INSERT INTO virtual  ('name', 'rom', 'type', 'device') VALUES ('Air_quality_PM10', 'Airly', 'airqualitypm10', 'virtual')";
+$updates['2018-03-09 09:40:04'][]="INSERT INTO virtual  ('name', 'rom', 'type', 'device') VALUES ('Air_quality_PM2.5', 'Airly25', 'air_pm_2.5', 'virtual')";
+$updates['2018-03-09 09:40:04'][]="INSERT INTO virtual  ('name', 'rom', 'type', 'device') VALUES ('Air_quality_PM10', 'Airly10', 'air_pm_10', 'virtual')";
 
 ?>
