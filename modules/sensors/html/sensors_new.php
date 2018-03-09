a48386c64cf7747101c29e3bbd3de637ef4c6cb3
mariano78 <mariano78@interia.pl>
2018-03-09 09:44:22 +0100

    v

--- modules/sensors/html/sensors_new.php
+++ modules/sensors/html/sensors_new.php
@@ -196,6 +196,28 @@ foreach ($result as $a) {
 </thead>
 <tbody>
 
+<?php	
+$db = new PDO('sqlite:dbf/nettemp.db');
+$sth = $db->prepare("SELECT * FROM virtual");
+$sth->execute();
+$resultv = $sth->fetchAll();
+foreach ($result as $v) {
+?>
+<tr>
+	<td class="col-md-0">
+		<?php echo $v['name']; ?>
+	</td>
+	<td class="col-md-0">
+		<?php echo $v['rom']; ?>
+	</td>
+	<td class="col-md-0">
+		<?php echo $v['type']; ?>
+	</td>
+	<td class="col-md-0">
+		<?php echo $v['device']; ?>
+	</td>
+	
+
 <tr>
 	<td  colspan = "12">
 		<center>
