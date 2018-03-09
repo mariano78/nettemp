2bd464c1704f14a1fbad876e2a7229fd6bfc39d8
mariano78 <mariano78@interia.pl>
2018-03-09 14:56:25 +0100

    v

--- modules/sensors/virtual.php
+++ modules/sensors/virtual.php
@@ -34,7 +34,7 @@ try {
 			
 			
 	//$json = file_get_contents('https://airapi.airly.eu/v1/nearestSensor/measurements?latitude=50.76197&longitude=19.0624&maxDistance=1000&apikey=dbad863c9c9c4516b8fcc33898b51393');
-	$url = "https://airapi.airly.eu/v1/nearestSensor/measurements?latitude=$lati&longitude=$long&maxDistance=1000&apikey=$api"
+	$url = "https://airapi.airly.eu/v1/nearestSensor/measurements?latitude=$lati&longitude=$long&maxDistance=1000&apikey=$api";
 	$json = file_get_contents($url);
 	
 	$obj = json_decode($json,true);
