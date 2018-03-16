23e22e8b6d4055afb97c99a1d894a08080ed0ea7
mariano78 <mariano78@interia.pl>
2018-03-16 16:49:46 +0100

    s

--- receiver.php
+++ receiver.php
@@ -187,13 +187,13 @@ function trigger($rom, $val) {
 			$msg = $date." - ".$name." - ALARM";
 			$sms = "To: ".$smsto[$x]."\n\n".$msg;
 			
-			$filepath = "tmp/sms/message_".$date.$random.".sms";
+			$filepath = "tmp/sms/message_".$date."_".$random.".sms";
 			$fsms = fopen($filepath, 'a+');
 			
 			fwrite($fsms, $sms);
 			fclose($fsms);
 			
-			$ftosend = "/var/spool/sms/outgoing/message_".$date.$random.".sms";
+			$ftosend = "/var/spool/sms/outgoing/message_".$date."_".$random.".sms";
 	
 			if (!copy($filepath, $ftosend)) {
 			echo "Send failed.\n";
@@ -230,13 +230,13 @@ function trigger($rom, $val) {
 			$msg = $date." - ".$name." - RECOVERY";
 			$sms = "To: ".$smsto[$x]."\n\n".$msg;
 			
-			$filepath = "tmp/sms/message_".$date.$random.".sms";
+			$filepath = "tmp/sms/message_".$date."_".$random.".sms";
 			$fsms = fopen($filepath, 'a+');
 			
 			fwrite($fsms, $sms);
 			fclose($fsms);
 			
-			$ftosend = "/var/spool/sms/outgoing/message_".$date.$random.".sms";
+			$ftosend = "/var/spool/sms/outgoing/message_".$date."_".$random.".sms";
 	
 			if (!copy($filepath, $ftosend)) {
 			echo "Send failed.\n";
