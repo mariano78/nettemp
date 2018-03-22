20ca1be2362cf2507e5164e244ca8834aec7cac3
mariano78 <mariano78@interia.pl>
2018-03-22 11:17:21 +0100

    h

--- status/sensor_groups.php
+++ status/sensor_groups.php
@@ -64,7 +64,7 @@ foreach ($row_meteo as $a) {
 		<input type="hidden" name="hidegstate" value="<?php echo $hide; ?>" />
 		
 		<input type="hidden" name="hidegroup" value="hidegroup"/>
-		<button><span style="float:right" class="glyphicon glyphicon-menu-down"></span> </button>
+		<button class="glyphicon"><span style="float:right" class="glyphicon glyphicon-menu-down"></span> </button>
     </form>	
 	
 	</div>
