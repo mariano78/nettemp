76adb60781095fb96ff168a7e15b33c9a1966956
mariano78 <mariano78@interia.pl>
2018-03-22 12:17:38 +0100

    h

--- status/sensor_groups.php
+++ status/sensor_groups.php
@@ -67,10 +67,10 @@ foreach ($row_meteo as $a) {
 		<?php
 		
 		if($hide =='off'){ ?>
-		<button type="submit" class="btn btn-link"><span style="float:right" class="glyphicon glyphicon-menu-up">AAA</span> </button>
+		<button type="submit" class="btn-link"><span style="float:right" class="glyphicon glyphicon-menu-up">AAA</span> </button>
 		
 		<?php } elseif($hide =='on'){?>
-		<button type="submit" class="btn btn-link"><span style="float:right" class="glyphicon glyphicon-menu-down">BBB</span> </button>
+		<button type="submit" class="btn-link"><span style="float:right" class="glyphicon glyphicon-menu-down">BBB</span> </button>
 		<?php } ?>
     </form>	
 	
