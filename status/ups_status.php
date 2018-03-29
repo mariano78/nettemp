f9ba21c163722b5cd9cbecb102b7f051b604275e
mariano78 <mariano78@interia.pl>
2018-03-29 10:03:21 +0200

    u

--- status/ups_status.php
+++ status/ups_status.php
@@ -7,11 +7,7 @@ foreach ($upsqr as $ups) {
 
 if ($nts_ups_status != 'on' ) { return; }
 else {
-	exec("/sbin/apcaccess",$upso);
-	foreach($upso as $ar) {
-	    $col = explode(":", $ar);
-	    $array[$col[0]]=$col[1];
-    	}
+	
 
 ?>
 
@@ -19,12 +15,30 @@ else {
 <div class="grid-item ups">
     <div class="panel panel-default">
     <div class="panel-heading">UPS Status</div>
-        <div class="panel-body">
+	
+	<div class="table-responsive">
+	<table class="table table-hover table-condensed">
+		<tbody>      
 <?php
+
+
+	exec("/sbin/apcaccess",$upso);
+	foreach($upso as $ar) {
+	    $col = explode(":", $ar);
+	    $array[$col[0]]=$col[1];
+    	}
+		
 foreach($array as $key => $value){
-    if (strpos($key, 'MODEL') !== false) {
-        echo "Model: ".$value."<br>";
-    }
+	
+	if (strpos($key, 'MODEL') !== false) { ?>
+	<tr>
+	<td>Model:</td>
+	<td><?php echo $value; ?></td>
+    </tr>
+	<?php }
+	
+	
+	
     if (strpos($key, 'STATUS') !== false) {
 	echo "Status: ".$value."<br>";
     }
@@ -40,16 +54,16 @@ foreach($array as $key => $value){
 	if (strpos($key, 'LOADPCT') !== false) {
 	echo "Load: ".$value."<br>";
     }
-	
-	
-	
-	
     if (strpos($key, 'TONBATT') !== false) {
 	echo "Time on baterry: ".$value."<br>";
     }
 
 }
-?>			
+?>
+
+</tbody>
+</table>
+</div>			
 	</div>
     </div>
 </div>
