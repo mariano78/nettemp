b603c32e64d83fb5f494f862e9e3aa6d45dcca6e
mariano78 <mariano78@interia.pl>
2018-04-12 10:27:55 +0200

    z

--- modules/mqtt/publish.php
+++ modules/mqtt/publish.php
@@ -12,7 +12,7 @@ $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to 
 $mqtt = new phpMQTT($server, $port, $client_id);
 
 if ($mqtt->connect(true, NULL, $username, $password)) {
-	$mqtt->publish("/Kominek/GPIO/12", "1" . date("r"), 0);
+	$mqtt->publish("/Kominek/LED/gpio", "1" . date("r"), 0);
 	$mqtt->close();
 } else {
     echo "Time out!\n";
