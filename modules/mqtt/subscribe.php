d836240c099ace8b6db835377f717c17d486b2f0
mariano78 <mariano78@interia.pl>
2018-04-13 13:09:10 +0200

    z

--- modules/mqtt/subscribe.php
+++ modules/mqtt/subscribe.php
@@ -44,7 +44,7 @@ $mqtt->close();
 
 
 function procmsg($topic, $msg){
-	//	echo "Msg Recieved: " . date("r") . "\n";
+		echo "Msg Recieved:\n";
 		echo "Topic: {$topic}\n\n";
 	//	echo "\t$msg\n\n";
 
@@ -95,7 +95,7 @@ function procmsg($topic, $msg){
 	$local_val	=	$output;
 	$local_name	=	$name;
 	$local_ip	=	$ip;
-	$local_rom=$local_device."_".$local_ip."_".$local_name."_".$local_type;
+	$local_rom=$local_device."_".$local_name."_".$local_type;
     }
     echo $date." Rom:".$local_rom." Name: ".$local_name." Value: ".$output." IP: ".$local_ip." GPIO: ".$local_gpio."\n";
     db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
