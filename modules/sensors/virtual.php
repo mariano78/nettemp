8b885121dc794cd6bdbc588c083b5f6ebd6b07ab
mariano78 <mariano78@interia.pl>
2018-07-25 13:17:46 +0200

    ss

--- modules/sensors/virtual.php
+++ modules/sensors/virtual.php
@@ -103,8 +103,8 @@ try {
 			$local_device = $vr['device'];
 			
 			if ($local_type == "sunrise"){	
-				$local_val2 = (date_sunrise(time(),SUNFUNCS_RET_STRING,$lati,$long,90.83,$tz));
-				$local_val = str_replace(":", "", $local_val2);
+				$local_val = (date_sunrise(time(),SUNFUNCS_RET_DOUBLE,$lati,$long,90.83,$tz));
+				//$local_val = str_replace(":", "", $local_val2);
 			} elseif  ($local_type == "sunset"){
 				$local_val = (date_sunset(time(),SUNFUNCS_RET_STRING,$lati,$long,90.83,$tz));	
 			}
