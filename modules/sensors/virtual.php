61d42a41ba5e669429e7470761699228044d5e2d
mariano78 <mariano78@interia.pl>
2018-07-25 13:36:30 +0200

    ssss

--- modules/sensors/virtual.php
+++ modules/sensors/virtual.php
@@ -113,7 +113,7 @@ try {
 		echo $local_val."\n";
 		
 		
-		echo date_format($local_val, 'H:i')."\n";
+		echo date('H:i', $local_val)."\n";
 		echo $local_type."\n";
 		db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);	
 
