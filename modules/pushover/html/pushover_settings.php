f3a68762572ebd0d0e6ae351528041b18e2ccad6
Mariusz Musik <m@serwer-robelit.local>
2018-09-11 21:23:21 +0200

    s

--- modules/pushover/html/pushover_settings.php
+++ modules/pushover/html/pushover_settings.php
@@ -14,8 +14,8 @@
 				<td><label>Active:</label></td>
 				<td>
 					<form action="" method="post">
-						<input data-toggle="toggle" data-size="mini" onchange="this.form.submit()"  type="checkbox" name="ms_onoff" value="on" <?php echo $nts_mail_onoff == 'on' ? 'checked="checked"' : ''; ?>  />
-						<input type="hidden" name="ms_onoff1" value="ms_onoff2" />
+						<input data-toggle="toggle" data-size="mini" onchange="this.form.submit()"  type="checkbox" name="ms_onoff" value="on" <?php echo $nts_pusho_active == 'on' ? 'checked="checked"' : ''; ?>  />
+						<input type="hidden" name="po_onoff1" value="po_onoff2" />
 					</form>
 				</td>
 			</tr>
@@ -23,14 +23,14 @@
 			<tr>
 				<td><label>User KEY:</label></td>
 				<td>
-					<input id="user" name="address" placeholder="not required" class="form-control input-md" type="text" value="<?php echo $a['from']; ?>">
+					<input id="user" name="address" class="form-control input-md" type="text" value="<?php echo $a['from']; ?>">
 				</td>
 			</tr>
 			
 			<tr>
 				<td><label>API KEY:</label></td>
 				<td>
-					<input id="user" name="user" placeholder="ex. nettemp@nettemp.pl" class="form-control input-md" required="" type="text" value="<?php echo $a['user']; ?>">
+					<input id="user" name="user"  class="form-control input-md" required="" type="text" value="<?php echo $a['user']; ?>">
 				</td>
 			</tr>
 			
@@ -38,7 +38,7 @@
 			<tr>
 				<td></td>
 				<td>
-					<input type="hidden" name="change_password1" value="change_password2" />
+					<input type="hidden" name="chang" value="change_password2" />
 					<button id="mailsave" name="mailsave" class="btn btn-xs btn-success">Save</button>
 		</form>
 				</td>
