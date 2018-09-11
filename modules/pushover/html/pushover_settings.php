4a896a5abfad0de0234044a3903c580ad8df9f63
Mariusz Musik <m@serwer-robelit.local>
2018-09-11 19:05:54 +0200

    s

--- modules/pushover/html/pushover_settings.php
+++ modules/pushover/html/pushover_settings.php
@@ -1,85 +1,10 @@
 <?php
-$cfile = '/etc/msmtprc';
 
-$fh = fopen($cfile, 'r');
-$theData = fread($fh, filesize($cfile));
-$cread = array();
-$my_array = explode(PHP_EOL, $theData);
-foreach($my_array as $line)
-{
-    $tmp = explode(" ", $line);
-    $cread[$tmp[0]] = $tmp[1];
-}
-fclose($fh);
-$a=$cread;
-
-
-$db = new PDO('sqlite:dbf/nettemp.db');
-$ns_row = $db->query("SELECT value FROM nt_settings WHERE option='mail_topic'") or header("Location: html/errors/db_error.php");
-$ns_rows = $ns_row->fetchAll();
-
-foreach ($ns_rows as $v) { 	
-	$mail_topic=$v['value'];
-}
-
-
-$address = isset($_POST['address']) ? $_POST['address'] : '';
-$user = isset($_POST['user']) ? $_POST['user'] : '';
-$host = isset($_POST['host']) ? $_POST['host'] : '';
-$port = isset($_POST['port']) ? $_POST['port'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
-$auth = isset($_POST['auth']) ? $_POST['auth'] : '';
-$tls = isset($_POST['tls']) ? $_POST['tls'] : '';
-$tlscheck = isset($_POST['tlscheck']) ? $_POST['tlscheck'] : '';
-$topic = isset($_POST['topic']) ? $_POST['topic'] : '';
-
-
-
-$change_password1 = isset($_POST['change_password1']) ? $_POST['change_password1'] : '';
-if  ($change_password1 == "change_password2") {
-	if (!file_exists($cfile)) {
-		$cmd = "sudo touch $cfile && sudo chown www-data $cfile && sudo chmod 600 $cfile";
-		shell_exec($cmd);
-	
-	}
-		$fh = fopen($cfile, 'w');
-
-		if(empty($address)||$address=='default'){
-			$address=$user;
-		}
-
-$conf = array (
-    'defaults' => '', 
-    'tls' => "$tls",
-    'tls_starttls' => "$tls",
- // 'tls_trust_file' => '/etc/ssl/certs/ca-certificates.crt',
-    'tls_certcheck' => "$tlscheck",
-    'account' => 'default',
-	 'host' => "$host",
-	 'port' => "$port",
-	 'auth' => "$auth",
-	 'user' => "$user",
-	 'password' => "$password",
-	 'from' => "$address",
-	 'logfile' => '/var/log/msmtp.log'
-    );
-  
-
-		foreach ($conf as $index => $string) {
-    		fwrite($fh, $index." ".$string."\n");
-		}
-		
-
-		$db->exec("UPDATE nt_settings SET value='$topic' WHERE option='mail_topic'") or die ($db->lastErrorMsg());
-		
-		header("location: " . $_SERVER['REQUEST_URI']);
-    	exit();
-}
 ?>
 
 <div class="grid-item">
 		<div class="panel panel-default">
-			<div class="panel-heading">Pushover (Android / IOs)</div>
+			<div class="panel-heading">Pushover (Android / iOs)</div>
 			
 		<div class="table-responsive">
 		<table class="table table-hover table-condensed">
