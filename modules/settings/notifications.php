2767bae27d4ca310fc8200a2104992c8a80bc3e0
Mariusz Musik <m@serwer-robelit.local>
2018-09-11 15:35:08 +0200

    s

--- modules/settings/notifications.php
+++ modules/settings/notifications.php
@@ -201,14 +201,6 @@ $conf = array (
 					<button id="send" name="send" value="send" class="btn btn-xs btn-success">Send test</button>
 				
 				</form>
-				
-				</td>
-			</tr>
-			<tr>
-				<td></td>
-				
-				
-				<td>
 				<?php
 $test_mail = isset($_POST['test_mail']) ? $_POST['test_mail'] : '';
 $send = isset($_POST['send']) ? $_POST['send'] : '';
@@ -234,6 +226,14 @@ if  ($send == "send") {
 }
 
 ?>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				
+				
+				<td>
+				
 				
 				</td>
 			</tr>
