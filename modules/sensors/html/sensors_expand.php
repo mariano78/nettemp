e379501afbeb7b11594857e7651074f00b4b31ad
Mariusz Musik <m@serwer-robelit.local>
2018-09-24 11:25:36 +0200

    d

--- modules/sensors/html/sensors_expand.php
+++ modules/sensors/html/sensors_expand.php
@@ -7,6 +7,7 @@
 	<tr>
 		<th>Adjust</th>
 		<th>Thing Speak</th>
+		<th>Remote NT</th>
 	
 	</tr>
 </thead>
@@ -33,6 +34,19 @@
 			<?php echo $a["thing"] == 'on' ? 'ON' : 'OFF'; ?></button>
 			<input type="hidden" name="th_on" value="th_on" />
 		</form>
+		</td>
+		
+		<td class="col-md-0">
+		<?php if ($a["device"] != 'remote' && $a["device"] != 'gpio') { ?>
+		<form action="" method="post" style="display:inline!important;"> 	
+			<input type="hidden" name="remote" value="<?php echo $a["id"]; ?>" />
+			<button type="submit" name="remoteon" value="<?php echo $a["remote"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["remote"] == 'on' ? 'class="btn btn-xs btn-primary"' : 'class="btn btn-xs btn-default"'; ?>>
+			<?php echo $a["remote"] == 'on' ? 'ON' : 'OFF'; ?></button>
+			<input type="hidden" name="remoteonoff" value="onoff" />
+		</form>
+		<?php 
+		}
+		?>
 		</td>
 	</tr>
 	
