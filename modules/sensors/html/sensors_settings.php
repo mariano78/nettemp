8e40546e07e2a21e63acc921cf3bed5a2c146bf0
Mariusz Musik <m@serwer-robelit.local>
2018-09-24 10:21:01 +0200

    d

--- modules/sensors/html/sensors_settings.php
+++ modules/sensors/html/sensors_settings.php
@@ -834,7 +834,7 @@ $row = $rows->fetchAll();
     </td>
 	<td>
 	
-	<a href="index.php?id=<?php echo $id ?>&type=devices&device_group=<?php echo $device_group?>&device_type=<?php echo $t_g?>&device_menu=expand_device&device_rom=<?php echo $a["rom"]; ?>" ><button class="btn btn-xs btn-default">Settings</button></a>
+	<a href="index.php?id=<?php echo $id ?>&type=devices&device_group=<?php echo $device_group?>&device_type=<?php echo $device_type?>&device_menu=expand_device&device_rom=<?php echo $a["rom"]; ?>" ><button class="btn btn-xs btn-default">Settings</button></a>
 	
 	</td>
     
