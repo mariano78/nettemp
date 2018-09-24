31caf7a04a55a2397424b04d9db10a9d4c85c285
Mariusz Musik <m@serwer-robelit.local>
2018-09-24 13:43:10 +0200

    d

--- modules/sensors/html/sensors_notifications.php
+++ modules/sensors/html/sensors_notifications.php
@@ -19,6 +19,7 @@
 	
 </tbody>
 </table>
-<a href="index.php?id=<?php echo $id ?>&type=devices&device_group=<?php echo $device_group?>&device_type=<?php echo $device_type?>&device_menu=settings&device id=<?php  if($device_group == '' && $device_type == '' && $device_id == '') {echo '';} else {echo $a["id"];} ?>" ><button class="btn btn-xs btn-info">Back</button></a>
+
 </div>
+<a href="index.php?id=<?php echo $id ?>&type=devices&device_group=<?php echo $device_group?>&device_type=<?php echo $device_type?>&device_menu=settings&device id=<?php  if($device_group == '' && $device_type == '' && $device_id == '') {echo '';} else {echo $a["id"];} ?>" ><button class="btn btn-xs btn-info">Back</button></a>
 </div>
