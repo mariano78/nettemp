abd13fa9cba1eb1643f94ae1c33b792c3686d49b
Mariusz Musik <m@serwer-robelit.local>
2018-09-24 11:22:34 +0200

    d

--- modules/sensors/html/sensors_expand.php
+++ modules/sensors/html/sensors_expand.php
@@ -6,6 +6,7 @@
 <thead>
 	<tr>
 		<th>Adjust</th>
+		<th>Thing Speak</th>
 	
 	</tr>
 </thead>
@@ -25,6 +26,19 @@
 		?>
 		</td>
 	</tr>
+	
+	<tr>
+		<td class="col-md-0">
+		<form action="" method="post" style="display:inline!important;" > 	
+			<input type="hidden" name="thing_id" value="<?php echo $a["id"]; ?>" />
+			<button type="submit" name="thing_on" value="<?php echo $a["thing"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["thing"] == 'on' ? 'class="btn btn-xs btn-primary"' : 'class="btn btn-xs btn-default"'; ?>>
+			<?php echo $a["thing"] == 'on' ? 'ON' : 'OFF'; ?></button>
+			<input type="hidden" name="th_on" value="th_on" />
+		</form>
+		</td>
+	</tr>
+	
+	
 
 </tbody>
 </table>
