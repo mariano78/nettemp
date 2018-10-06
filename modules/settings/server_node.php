32d8185592cd1115a1fdef6d2c5fae25e309a301
Mariusz Musik <m@serwer-robelit.local>
2018-10-06 17:44:31 +0200

    aa

--- modules/settings/server_node.php
+++ modules/settings/server_node.php
@@ -250,6 +250,47 @@ if ($cauth_on == 'on'){
 </fieldset>
 </form>
 
+<form action="" method="post">
+    <input data-on="AUTH" data-off="AUTH" data-toggle="toggle" data-size="mini" onchange="this.form.submit()"  type="checkbox" name="cauth_on" value="on" <?php echo $cauth_on == 'on' ? 'checked="checked"' : ''; ?>  />
+    <input type="hidden" name="cauth_onoff" value="cauth_onoff" />
+</form>
+<?php
+if ($nts_domo_auth == 'on'){
+?>
+
+<form action="" method="post" class="form-horizontal">
+<fieldset>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textinput">User:</label>  
+  <div class="col-md-4">
+  <input id="textinput" name="cauth_login" placeholder="" class="form-control input-md" required="" type="text" value="admin" disabled>
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textinput">Password:</label>  
+  <div class="col-md-4">
+  <input id="textinput" name="cauth_pass" placeholder="" class="form-control input-md" required="" type="password" value="<?php echo $cauth_pass; ?>">
+     <input type="hidden" name="cauth_save" value="cauth_save" />
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button id="singlebutton" name="singlebutton" class="btn btn-xs btn-success">Save</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+<?php
+	}
+}
+?>
+
 
 </div>
 </div>
