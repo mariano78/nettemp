97acf36dce89c93e46eae299eb140a27f5559eed
Mariusz Musik <m@serwer-robelit.local>
2018-10-12 14:05:52 +0200

    Update modules/sensors/html/sensors_notifications.php

--- modules/sensors/html/sensors_notifications.php
+++ modules/sensors/html/sensors_notifications.php
@@ -146,19 +146,19 @@ if(!empty($nrom) && ($nadd == "nadd")) {
 	<tr>
 		<td><label>SMS:</label></td>
 		<td>
-			<input type="checkbox" data-toggle="toggle" data-size="mini"  name="smsonoff">
+			<input type="checkbox" value="" data-toggle="toggle" data-size="mini"  name="smsonoff">
 		</td>
 	</tr>
 	<tr>
 		<td><label>Mail:</label></td>
 		<td>
-			<input type="checkbox" data-toggle="toggle" data-size="mini"  name="mailonoff">
+			<input type="checkbox" value="" data-toggle="toggle" data-size="mini"  name="mailonoff">
 		</td>
 	</tr>
 	<tr>
 		<td><label>PushOver:</label></td>
 		<td>
-			<input type="checkbox" data-toggle="toggle" data-size="mini"  name="poonoff">
+			<input type="checkbox" value="" data-toggle="toggle" data-size="mini"  name="poonoff">
 		</td>
 	</tr>
 	<tr>
@@ -182,13 +182,13 @@ if(!empty($nrom) && ($nadd == "nadd")) {
 	<tr>
 		<td><label>Ignore interval:</label></td>
 		<td>
-			<input type="checkbox" data-toggle="toggle" data-size="mini"  name="intervalonoff">
+			<input type="checkbox" value="" data-toggle="toggle" data-size="mini"  name="intervalonoff">
 		</td>
 	</tr>
 	<tr>
 		<td><label>Recovery:</label></td>
 		<td>
-			<input type="checkbox" data-toggle="toggle" data-size="mini"  name="recoveryonoff">
+			<input type="checkbox" value="" data-toggle="toggle" data-size="mini"  name="recoveryonoff">
 		</td>
 	</tr>
 	<tr>
