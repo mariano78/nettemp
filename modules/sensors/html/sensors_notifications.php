87b1ba354301f79e48ca11144af9115a4c1cf641
mariano78 <mariano78@interia.pl>
2018-11-27 13:16:26 +0100

    Update modules/sensors/html/sensors_notifications.php

--- modules/sensors/html/sensors_notifications.php
+++ modules/sensors/html/sensors_notifications.php
@@ -23,7 +23,7 @@ if(!empty($nrom) && ($nadd == "nadd")) {
 
 
 $db = new PDO("sqlite:$root/dbf/nettemp.db");	
-$notif = $db->query("SELECT * FROM notifications");
+$notif = $db->query("SELECT * FROM notifications WHERE rom=$a["rom"];");
 $notifs = $notif->fetchAll();	
 
 ?>
