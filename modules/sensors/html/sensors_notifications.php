8da41cf302872af872250f4a2f9b6cb5496b59d7
mariano78 <mariano78@interia.pl>
2018-11-27 15:43:22 +0100

    Update sensors_notifications.php

--- modules/sensors/html/sensors_notifications.php
+++ modules/sensors/html/sensors_notifications.php
@@ -260,18 +260,7 @@ $notifs = $notif->fetchAll();
 
 </table>
 </div>
-<table class="table table-hover table-condensed small">
-<thead>
-</thead>
-<tbody>
-<tr>
-<td>
-<a href="index.php?id=<?php echo $id ?>&type=devices&device_group=<?php echo $device_group?>&device_type=<?php echo $device_type?>&device_menu=settings&device id=<?php  if($device_group == '' && $device_type == '' && $device_id == '') {echo '';} else {echo $a["id"];} ?>" ><button class="btn btn-xs btn-info">Back</button></a>
-</td>
-</tr>
-</tbody>
 
-</table>
 </div>
 
 <div class="panel panel-default">
