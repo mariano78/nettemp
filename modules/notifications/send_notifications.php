99948c86401154f254a9f317611523fdcaa43bec
mariano78 <mariano78@interia.pl>
2018-11-29 14:15:56 +0100

    Update send_notifications.php

--- modules/notifications/send_notifications.php
+++ modules/notifications/send_notifications.php
@@ -17,10 +17,59 @@ try {
 	$query = $db->query("SELECT * FROM notifications WHERE active='on'");
     $result= $query->fetchAll();
     
-    foreach($result as $s) {
+    foreach($result as $sn) {
 		
+		$nrom=$sn[rom];
+		$ntype=$sn[type];
+		$nwhen=$sn[wheen];
+		$nvalue=$sn[value];
+		$nsms=$sn[sms];
+		$nmail=$sn[mail];
+		$npov=$sn[pov];
+		$nmsg=$sn[message];
+		$npriority=$sn[priority];
+		$niginterval=$sn[iginterval];
+		$nrecovery=$sn[recovery];
 		
+		$sensor = $db->query("SELECT name,tmp,current,type FROM sensors WHERE rom='$nrom'");
+		$sensors = $sensor->fetchAll();
+		
+		foreach ($sensors as $sen) {
+			
+			$sname=$sen[name];
+			$stmp=$sen[tmp];
+			$scurrent=$sen[current];
+			$stype=$sen[type];
+			
+		}
+		
+//sms	
+		if ($nsms == "on") {
+			
+			
+		}
+// sms end
+
+//mail	
+		if ($nmail == "on") {
+			
+			
+		}
+//mail end
+	
+//poshover	
+		if ($npov == "on") {
+			
+			
+		}
+//poshover end	
+
+	
+	
 	}
+	
+	
+//try end	
 } catch (Exception $e) {
     echo $date." Error\n";
     exit;
