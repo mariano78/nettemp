8f76a7fb06f1b8c4f7e00bc1b1014d2cd7d94e61
mariano78 <mariano78@interia.pl>
2018-12-12 11:01:45 +0100

    Update send_notifications.php

--- modules/notifications/send_notifications.php
+++ modules/notifications/send_notifications.php
@@ -254,7 +254,7 @@ try {
 
 			if ($nwhen == '1') {
 				
-				if (($stmp < $nvalue) && $nsent != 'sent') {
+				if (($stmp < $nvalue) && $nsent == '') {
 					
 					$notsent = 1;
 					echo "aa\n";
