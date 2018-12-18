4b8e53ff7483004105077c785250b90135f11e65
mariano78 <mariano78@interia.pl>
2018-12-18 15:30:43 +0100

    Update sensors_settings.php

--- modules/sensors/html/sensors_settings.php
+++ modules/sensors/html/sensors_settings.php
@@ -534,7 +534,7 @@ $row = $rows->fetchAll();
 	
     <td class="col-md-0">
     <form action="" method="post" style="display:inline!important;">
-		<input type="text" name="name_new" size="10" maxlength="30" value="<?php echo $a["name"]; ?>" />
+		<input type="text" name="name_new" size="8" maxlength="30" value="<?php echo $a["name"]; ?>" />
 		<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
 		<input type="hidden" name="name_id" value="<?php echo $a["id"]; ?>" />
 		<input type="hidden" name="gpio" value="<?php echo $a["gpio"]; ?>" />
