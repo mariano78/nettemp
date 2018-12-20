46ba94b9ac7ae66dd4610aa2578c92e7cb705841
mariano78 <mariano78@interia.pl>
2018-12-20 14:40:47 +0100

    Update functions.php

--- common/functions.php
+++ common/functions.php
@@ -2,6 +2,8 @@
 $logroot = "/var/www/nettemp";
 function logs($date,$type,$message)
 	{
+	$logroot = "/var/www/nettemp";	
+		
 	$db = new PDO("sqlite:$logroot/dbf/nettemp.db");
 	$db->exec("INSERT INTO logs ('date', 'type', 'message') VALUES ('$date', '$type', '$message')");
 	}
