12e761c4ceb06756a3728c5139897800b8f99e20
mariano78 <mariano78@interia.pl>
2018-12-20 14:22:33 +0100

    Update log_nettemp.php

--- modules/tools/html/log/log_nettemp.php
+++ modules/tools/html/log/log_nettemp.php
@@ -13,7 +13,7 @@ $log_del = isset($_POST['log_del']) ? $_POST['log_del'] : '';
 
 	 ?>	
 <div class="panel panel-default">
-<div class="panel-heading">Logs</div>
+<div class="panel-heading">Logs <?php> echo $ROOT; ?></div>
 <div class="panel-body">
 
 <form action="index.php?id=tools&type=log" method="post">
