cfcd9d9a128c04f612f1a53ed7fa4dbbba3811f2
mariano78 <mariano78@interia.pl>
2018-12-26 10:52:33 +0100

    Update log_nettemp.php

--- modules/tools/html/log/log_nettemp.php
+++ modules/tools/html/log/log_nettemp.php
@@ -58,9 +58,11 @@ $last = array_slice($filearray,-100);
 $('#logs').scrollTop($('#logs')[0].scrollHeight);
 
     setInterval( function() {
-		$('#logs').load('http://31.6.65.11:8888/index.php?id=tools&type=log #logs');
+		//$('#logs').load('http://31.6.65.11:8888/index.php?id=tools&type=log #logs');
 		
-		$('#logs').scrollTop($('#logs')[0].scrollHeight);
+		$("#logs").load(location.href+" #logs>*",""); 
+		
+		//$('#logs').scrollTop($('#logs')[0].scrollHeight);
 		
     
    
