b805bf6ea6f6b0de61a09542e3cee8755c9a652a
mariano78 <mariano78@interia.pl>
2018-12-28 21:22:05 +0100

    Create log_auto_delete.php

---
+++ common/log_auto_delete.php
@@ -0,0 +1,22 @@
+
+<?php
+$root = "/var/www/nettemp";
+
+
+
+$db = new PDO("sqlite:$root/dbf/nettemp.db");
+
+$query = $db->query("SELECT * FROM nt_settings");
+    $result= $query->fetchAll();
+    
+    foreach($result as $s) {
+		
+		if($s['option']=='logshis') {
+			$logshistime=$s['value'];
+		}
+	}
+
+$db->exec("DELETE FROM logs WHERE time <= datetime('now','localtime','$logshistime')") or die ("No data to delete." );
+
+
+?>
