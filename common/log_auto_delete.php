7ce403c9008a889abc9a5a35d41b75a1acce3276
mariano78 <mariano78@interia.pl>
2018-12-30 12:55:18 +0100

    Update log_auto_delete.php

--- common/log_auto_delete.php
+++ common/log_auto_delete.php
@@ -14,12 +14,7 @@ $query = $db->query("SELECT * FROM nt_settings");
 			$logshistime="-".$logshistime." days";
 		}
 	}
-<<<<<<< HEAD
-$db->exec("DELETE FROM logs WHERE date <= datetime('now','localtime','$logshistime')") or die ("No data to delete.\n" );
-=======
-
-$db->exec("DELETE FROM logs WHERE time <= datetime('now','localtime','$logshistime')") or die ("No data to delete." );
->>>>>>> parent of f5fd7cb4... Update log_auto_delete.php
+$db->exec("DELETE FROM logs WHERE date <= datetime('now','localtime','$logshistime')") or die ($db->lastErrorMsg());
 
 
 ?>
