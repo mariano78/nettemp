100bc66786c4558b9f93fa516b5c4648f12f43f3
mariano78 <mariano78@interia.pl>
2019-01-02 13:42:32 +0100

    Update counters_reports.php

--- modules/counters/html/counters_reports.php
+++ modules/counters/html/counters_reports.php
@@ -17,41 +17,18 @@ foreach ($row as $a) {
 <div class="table-responsive">
 <table class="table table-hover table-condensed small" border="0">
 <thead>
-<th>Name</th>
-<th>Type</th>
-<th>Counters</th>
-<th>Show in status</th>
 </thead>
 <tr>
     <td class="col-md-0">
-		<?php echo $a["name"]; ?>
-	</td>
-	<td class="col-md-0">
-		<?php echo $a["type"]; ?>
-	</td>
-	<td class="col-md-0">
-		<form action="" method="post" style="display:inline!important;">
-			<input type="text" name="sum" size="16" maxlength="30" value="<?php echo $a["sum"]; ?>" required=""/>
-			<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
-			<input type="hidden" name="id" value="<?php echo $a["id"]; ?>" />
-			<input type="hidden" name="sum1" value="sum2"/>
-    </form>
+		
 	</td>
 	    <!--NEW GROUP-->
 
     <td class="col-md-9">
-    <form action="" method="post"  class="form-inline">
-    <select name="ch_groupon" class="form-control input-sm small" onchange="this.form.submit()" style="width: 100px;" >
-		<option value="sensors"  <?php echo $a['ch_group'] == 'sensors' ? 'selected="selected"' : ''; ?>  >Yes</option>
-		<option value="none"  <?php echo $a['ch_group'] == 'none' ? 'selected="selected"' : ''; ?>  >No</option>
-    </select>
-    <input type="hidden" name="ch_grouponoff" value="onoff" />
-    <input type="hidden" name="ch_group" value="<?php echo $a['id']; ?>" />
-    </form>
+    
     </td>
 </tr>
 <?php
-echo $crom;
 
 	}
 ?>
