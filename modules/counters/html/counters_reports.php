a08f24303ad06715d48f9a0b38686a563fcb7fd8
mariano78 <mariano78@interia.pl>
2019-01-07 11:27:20 +0100

    Update counters_reports.php

--- modules/counters/html/counters_reports.php
+++ modules/counters/html/counters_reports.php
@@ -96,7 +96,7 @@ $type = $a['type'];
 		?>
 		<tr>
 		
-			<td class="col-md-0"><label>Total:</label></td>
+			<td class="col-md-0"><h4><label>Total:</label></h4></td>
 			<td class="col-md-0"><label><?php echo $totalusage; ?></label></td>
 			<td class="col-md-0"><label><?php echo number_format($totalcosts, 2, '.', ''); ?></label></td>
 		
