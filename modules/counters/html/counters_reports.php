b05bc31c085a8a73d8ab7acba4dac98b69b938dc
mariano78 <mariano78@interia.pl>
2019-01-07 08:44:20 +0100

    Update counters_reports.php

--- modules/counters/html/counters_reports.php
+++ modules/counters/html/counters_reports.php
@@ -56,30 +56,22 @@ foreach ($row as $a) {
 			
 			</td>
 		</tr>
-		<tr>
 		
-			<form action="" method="post">	
-				<td>
+		<tr>
+			<td>
+				<form action="" method="post">	
 					<select name="repyear" id="ntype" >
 						<option value="2019" >2019</option>
 						<option value="2018" >2018</option>
 					</select>
+				</form>
 			</td>
-			</form>
+				
 		
 		</tr>
 		
 		<?php
 		}
-		
-		
-		?>
-	
-    
-    </td>
-
-<?php
-
 }
 ?>
 </table>
