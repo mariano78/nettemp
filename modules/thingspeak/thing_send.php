36b8222bc56f1f3c2d2cff9e12e600cd9020bea8
mariano78 <mariano78@interia.pl>
2019-01-09 11:29:19 +0100

    Update thing_send.php

--- modules/thingspeak/thing_send.php
+++ modules/thingspeak/thing_send.php
@@ -1,18 +1,21 @@
 <?php
 $ROOT=dirname(dirname(dirname(__FILE__)));
-//var_dump($argv);
+$date = date("Y-m-d H:i:s"); 
+include("$ROOT/common/functions.php");
+
+//var_dump($argv); logs($date,'Error','Cannot send mail because user doesnt have email, go to settings - users.'); 
 parse_str($argv[1],$params);
 $interval=$params['intv'];
 
 
-function logs($content){
-global $ROOT;
+//function logs($content){
+//global $ROOT;
 
-	$f = fopen("$ROOT/tmp/thingspeak_log.txt", "a");
+	//$f = fopen("$ROOT/tmp/thingspeak_log.txt", "a");
 
-fwrite($f, $content);
-fclose($f); 
-}
+//fwrite($f, $content);
+//fclose($f); 
+//}
 
 $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 
@@ -48,7 +51,7 @@ $row = $rows->fetchAll();
 			$content = $httpcode;
 			
 			$content = date('Y M d H:i:s')."-".$name."-".$data." - httpcode = ".$httpcode."\n";
-			logs($content);
+			logs($date,'Info',$content);
 
 	}
 
