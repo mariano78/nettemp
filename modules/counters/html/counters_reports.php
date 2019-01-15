5f89fa3fd3e08a791569b1ab11eb38fc6b77e1ab
mariano78 <mariano78@interia.pl>
2019-01-15 13:11:28 +0100

    Update counters_reports.php

--- modules/counters/html/counters_reports.php
+++ modules/counters/html/counters_reports.php
@@ -262,7 +262,7 @@ var chart = new Highcharts.Chart({
       },
 	  
 	  xAxis: {
-            categories: [<?php echo join('$xaxis', ',') ?>]
+            labels: [<?php echo join($xaxis, ',') ?>]
         },
 		
       series: [{
