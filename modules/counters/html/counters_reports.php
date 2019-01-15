4f8f008be83a47a452798d76d996ef99c80d9af9
mariano78 <mariano78@interia.pl>
2019-01-15 14:53:31 +0100

    Update counters_reports.php

--- modules/counters/html/counters_reports.php
+++ modules/counters/html/counters_reports.php
@@ -277,7 +277,7 @@ if ($theme == 'grid') { ?>
 
 <div class="row">
 <div class="col-md-10 col-md-offset-1">
-<div id="container" style="height: 700px; min-width: 310px padding: 20px"></div>
+<div id="container" style="height: 700px; min-width: 310px; padding: 20px"></div>
 
 <script type="text/javascript"> 
 var chart = new Highcharts.Chart({
