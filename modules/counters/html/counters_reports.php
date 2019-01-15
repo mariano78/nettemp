be46b5c5a0aaf7721dad79297e863c54bc3aa205
mariano78 <mariano78@interia.pl>
2019-01-15 08:26:39 +0100

    Update counters_reports.php

--- modules/counters/html/counters_reports.php
+++ modules/counters/html/counters_reports.php
@@ -61,8 +61,7 @@ $type = $a['type'];
 	<?php
 		$rom=$a['rom'];
 		$dbs = new PDO("sqlite:$root/db/$rom.sql") or die('lol');
-		$rows = $dbs->query("SELECT time AS date,round(sum(value),3) AS sums from def WHERE strftime('%Y',time) IN ('$repyearselect') GROUP BY strftime('%d',time)") or die('Something is wrong');
-		
+		$rows = $dbs->query("SELECT time AS date,round(sum(value),3) AS sums from def WHERE strftime('%Y',time) IN ('$repyearselect') GROUP BY strftime('%m',time), strftime('%d',time)") or die('Something is wrong');
 		
 		$row = $rows->fetchAll();
 		foreach ($row as $a) { 
