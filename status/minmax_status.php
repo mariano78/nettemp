a44ced01549d704b0a9d45c59f3390d01a499321
mariano78 <mariano78@interia.pl>
2019-04-17 10:45:19 +0200

    Update minmax_status.php

--- status/minmax_status.php
+++ status/minmax_status.php
@@ -123,9 +123,9 @@ $file=$rom .".sql";
 	} elseif ($a['type'] == 'sunrise' || $a['type'] == 'sunset') { ?>
     <tr>
 	<td><span class="label label-default"><?php echo str_replace("_", " ", $a['name']) ?></span></td>
-	<td><span class="label label-info"><?php echo date('H:i', $d['dmin'])?></span><span class="label label-warning"><?php echo number_format($d['dmax'], 2, '.', '')?></span></td>
-	<td><span class="label label-info"><?php echo number_format($w['wmin'], 2, '.', '')?></span><span class="label label-warning"><?php echo number_format($w['wmax'], 2, '.', '')?></span></td>
-	<td><span class="label label-info"><?php echo number_format($m['mmin'], 2, '.', '')?></span><span class="label label-warning"><?php echo number_format($m['mmax'], 2, '.', '')?></span></td>
+	<td><span class="label label-info"><?php echo date('H:i', $d['dmin'])?></span><span class="label label-warning"><?php echo date('H:i', $d['dmax'])?></span></td>
+	<td><span class="label label-info"><?php echo date('H:i', $w['wmin'])?></span><span class="label label-warning"><?php echo date('H:i', $w['wmax'])?></span></td>
+	<td><span class="label label-info"><?php echo date('H:i', $m['mmin'])?></span><span class="label label-warning"><?php echo date('H:i', $m['mmax'])?></span></td>
     </tr>
     <?php
      } else { ?>
