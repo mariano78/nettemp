4228e60d2fd018bf0823fc55d73d8a5642f56023
mariano78 <mariano78@interia.pl>
2019-05-07 13:18:29 +0200

    Update status_order.php

--- modules/settings/status_order.php
+++ modules/settings/status_order.php
@@ -1,5 +1,47 @@
+<?php //Modules ORDER
+
+ $modules = $db->query("SELECT * FROM statusorder ORDER BY position ASC") or header("Location: html/errors/db_error.php");
+	$morder = $modules->fetchAll();
+	
+
+?>
+
+
 <div class="grid-item settings">
 	<div class="panel panel-default">
-		<div class="panel-heading">Global settings</div>
+		<div class="panel-heading">Modules order</div>
+		
+		<table class="table table-hover table-condensed">
+		<tbody>
+		
+		<?php
+		
+			foreach($morder as $mor) {
+			$moduleposition = $mor['position'];
+			$modulename = $mor['modulename'];
+		?>
+		
+			<tr>
+			
+				<td><?php echo $moduleposition ?></td><td><?php echo $modulename ?></td>
+			
+			</tr>
+		<?php
+			}
+		
+		
+		
+		
+		?>
+		
+		
+		
+		</tbody>
+		</table>
+		
+		
+		
+		
+		
 	</div>
 </div>	
