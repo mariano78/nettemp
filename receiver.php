f7a5d92f66aec672059e386b0b3fa78bc7bf2b9f
mariano78 <mariano78@interia.pl>
2019-06-12 11:08:32 +0200

    Update receiver.php

--- receiver.php
+++ receiver.php
@@ -397,7 +397,7 @@ function db($rom,$val,$type,$device,$current,$ip,$gpio,$i2c,$usb,$name){
 		//if not in sensors table
 		else {
 			if  ($type == 'elecesp') {
-				$type = 'elec'
+				$type = 'elec';
 				}
 			$name=substr(rand(), 0, 4);
 			$dbr->exec("INSERT OR IGNORE INTO newdev (rom,type,device,ip,gpio,i2c,usb,name) VALUES ('$rom','$type','$device','$ip','$gpio','$i2c','$usb','$name')");
@@ -408,7 +408,7 @@ function db($rom,$val,$type,$device,$current,$ip,$gpio,$i2c,$usb,$name){
 	//if base not exist
 	else {
 		if  ($type == 'elecesp') {
-			$type = 'elec'
+			$type = 'elec';
 			}
 		$name=substr(rand(), 0, 4);
 		$dbr->exec("INSERT OR IGNORE INTO newdev (rom,type,device,ip,gpio,i2c,usb,name) VALUES ('$rom','$type','$device','$ip','$gpio','$i2c','$usb','$name')");
