d099660731b58de37d1d46833ef956eaaf9aba58
mariano78 <mariano78@interia.pl>
2019-07-01 13:23:20 +0200

    Delete po.bash

--- scripts/po.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-curl -s -F "token=ayjoka66mocbuqgb5oo42zs1xxdoia" \
-    -F "user=u8z2an68bkgdbhe22rq1bripbo1c34" \
-    -F "title=YOUR_TITLE_HERE" \
-    -F "message=$1" https://api.pushover.net/1/messages.json
