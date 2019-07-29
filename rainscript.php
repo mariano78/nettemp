68c9bb45e1e35d8aa14910814804306355a4d122
mariano78 <mariano78@interia.pl>
2019-07-29 12:26:13 +0200

    Update rainscript.php

--- rainscript.php
+++ rainscript.php
@@ -12,7 +12,7 @@ include("$ROOT/receiver.php");
 	  
 			$rain24h=$rainfall['rain24h'];
 			
-		db('rain24',$rain24h,'rainfall24h','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
+		db('rain24',$rain24h,'rainfall','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 	  }
 	  
 	  $query = $db2->query("SELECT sum(value) AS rain48h from def WHERE  time BETWEEN datetime('now', 'localtime', '-2 day') AND datetime('now', 'localtime')");
@@ -20,6 +20,7 @@ include("$ROOT/receiver.php");
 	  foreach($result as $rainfall) {
 	  
 			$rain48h=$rainfall['rain48h'];
+		db('rain48',$rain48h,'rainfall','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 	  }
 	  
 	  
@@ -28,6 +29,7 @@ include("$ROOT/receiver.php");
 	  foreach($result as $rainfall) {
 	  
 			$rain72h=$rainfall['rain72h'];
+			db('rain72',$rain72h,'rainfall','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 	  }
 	  
 	  $query = $db2->query("SELECT time FROM def WHERE value > 0 ORDER BY time DESC LIMIT 1");
@@ -37,6 +39,7 @@ include("$ROOT/receiver.php");
 			$rainlast=substr($rainfall['time'], 0, 10);
 			$rainlast2= substr($rainlast, 0, 10);
 			$rainlast2 = $rainlast2." 00:00:00";
+			db('rainlast',$rainlast2,'rainfalllast','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 			
 	  }
 	  
@@ -45,39 +48,9 @@ include("$ROOT/receiver.php");
 	  foreach($result as $rainfall) {
 	  
 			$rainlastsum=$rainfall['rainsum'];
+			db('rainlastsum',$rainlast2,'rainfallsum','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 	  }
 
       //db('rain24',$rain24h,'rainfall24h','virtual',$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 
     ?>
-	
-	
-	
-	
-	
-  <div style="width:100%">
-    <div style="width:70%;float:left" ><span>Ostatnie 24h</span></div>
-    <div style="width:30%;float:left;text-align: right;"><span><?php echo $rain24h; ?> l/m2</span></div>
-  </div>
-  
-  <div style="width:100%">
-    <div style="width:70%;float:left" ><span>Ostatnie 48h</span></div>
-    <div style="width:30%;float:left;text-align: right;"><span><?php echo $rain48h; ?> l/m2</span></div>
-  </div>
-  
-  <div style="width:100%">
-    <div style="width:70%;float:left" ><span>Ostatnie 72h</span></div>
-    <div style="width:30%;float:left;text-align: right;"><span><?php echo $rain72h; ?> l/m2</span></div>
-  </div>
-  
-  <div style="width:100%">
-    <div style="width:70%;float:left" ><span>Ostatni deszcz</span></div>
-    <div style="width:30%;float:left;text-align: right;"><span><?php echo $rainlast; ?></span></div>
-  </div>
-  
-   <div style="width:100%">
-    <div style="width:70%;float:left" ><span>Ostatni deszcz suma</span></div>
-    <div style="width:30%;float:left;text-align: right;"><span><?php echo $rainlastsum; ?>l/m2</span></div>
-  </div>
- 
-</div>
