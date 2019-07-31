8c6a0716fe56e370594461d0ff789eafae6bb83e
mariano78 <mariano78@interia.pl>
2019-07-31 10:35:47 +0200

    Update a.php

--- a.php
+++ a.php
@@ -1,11 +1,16 @@
 <?php
 
 $secret = 203730412;
-$url = '-i -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" -H "X-Ca_key: 203730412" -X GET http://api.general.zevercloud.cn/getPlantOverview?key= ksj6grior5bfv6r3m0xrkescxnxhvmxt';
+$url = '-i -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" -H "X-Ca_key: 203730412" -X GET http://api.general.zevercloud.cn/getPlantOverview?key=ksj6grior5bfv6r3m0xrkescxnxhvmxt';
 
-$urlsign = base64_encode(hash_hmac('sha256', $url, $secret, true));
+$url2 = 'GET#*/*##application/x-www-form-urlencoded:charset=UTF-8##/getPlantOverview?key=ksj6grior5bfv6r3m0xrkescxnxhvmxt';
+
+$urlsign = base64_encode(hash_hmac('sha256', $url2, $secret, true));
 
 echo $urlsign;
+
+
+
 
 
 
