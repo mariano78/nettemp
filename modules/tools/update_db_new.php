d0f5cfc7383d770bcd7107ed041251f845dcaff2
Mariusz Musik <m@serwer-robelit.local>
2019-08-23 11:31:10 +0200

    charts refresh

--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -232,6 +232,9 @@ $updates['2019-07-15 09:00:35'][]="UPDATE nt_settings SET value ='media/png/nett
 
 $updates['2019-08-21 07:00:35'][]="INSERT OR IGNORE INTO types (type, unit, unit2, ico, title, min, max) VALUES ('elecesp', 'kWh', 'W', 'media/ico/Lamp-icon.png' ,'Electricity','0', '99999999')";
 
+//Update - charts referesh
+$updates['2019-08-23 10:40:05'][]="INSERT INTO nt_settings ('option', 'value') VALUES ('chartsrefresh','off')";
+
 
 
 
