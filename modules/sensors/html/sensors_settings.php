02dcb312a01d00906318ee4ea3abb2517bea9ec7
Mariusz Musik <m@serwer-robelit.local>
2019-09-10 12:10:02 +0200

    Update sensors_settings.php

--- modules/sensors/html/sensors_settings.php
+++ modules/sensors/html/sensors_settings.php
@@ -384,23 +384,23 @@ if ( $lcd == "lcd"){
     }
 	
 	$dptempbindsensor = isset($_POST['dptempbindsensor']) ? $_POST['dptempbindsensor'] : '';
-    $dptemp_id = isset($_POST['dptemp_id']) ? $_POST['dptemp_id'] : '';
+    $dp_id = isset($_POST['dp_id']) ? $_POST['dp_id'] : '';
 	$cht_bsensor = isset($_POST['cht_bsensor']) ? $_POST['cht_bsensor'] : '';
 	
-	if (!empty($dptemp_id) && $cht_bsensor == "cht_bsensorok"){
+	if (!empty($dp_id) && $cht_bsensor == "cht_bsensorok"){
     $db = new PDO('sqlite:dbf/nettemp.db');
-    $db->exec("UPDATE sensors SET dpromtemp='$dptempbindsensor' WHERE id='$dptemp_id'") or die ($db->lastErrorMsg());
+    $db->exec("UPDATE sensors SET dpromtemp='$dptempbindsensor' WHERE id='$dp_id'") or die ($db->lastErrorMsg());
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
     } 
 	
 	$dphumbindsensor = isset($_POST['dphumbindsensor']) ? $_POST['dphumbindsensor'] : '';
-    $dphum_id = isset($_POST['dphum_id']) ? $_POST['dphum_id'] : '';
+    $dp_id = isset($_POST['dp_id']) ? $_POST['dp_id'] : '';
 	$chh_bsensor = isset($_POST['chh_bsensor']) ? $_POST['chh_bsensor'] : '';
 	
-	if (!empty($dphum_id) && $chh_bsensor == "cht_bsensorok"){
+	if (!empty($dp_id) && $chh_bsensor == "cht_bsensorok"){
     $db = new PDO('sqlite:dbf/nettemp.db');
-    $db->exec("UPDATE sensors SET dpromhumid='$dphumbindsensor' WHERE id='$dphum_id'") or die ($db->lastErrorMsg());
+    $db->exec("UPDATE sensors SET dpromhumid='$dphumbindsensor' WHERE id='$dp_id'") or die ($db->lastErrorMsg());
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
     } 
