f885c54fdfd6564046a0ea95a665c7db90a71b72
Mariusz Musik <m@serwer-robelit.local>
2019-09-10 11:28:57 +0200

    Update virtual.php

--- modules/sensors/virtual.php
+++ modules/sensors/virtual.php
@@ -189,7 +189,8 @@ try {
 			echo $humiddewpoint."\n";
 			
 				
-			$local_val=243.12*(((LOG10($humiddewpoint)-2)/0.4343)+(17.5*$tempdewpoint)/(243.12+$tempdewpoint))/(17.62-(((LOG10($humiddewpoint)-2)/0.4343)+(17.5*$tempdewpoint)/(243.12+$tempdewpoint)));
+			$val=243.12*(((LOG10($humiddewpoint)-2)/0.4343)+(17.5*$tempdewpoint)/(243.12+$tempdewpoint))/(17.62-(((LOG10($humiddewpoint)-2)/0.4343)+(17.5*$tempdewpoint)/(243.12+$tempdewpoint)));
+			$local_val = number_format ( $val, 2 );
 		
 		echo $local_rom."\n";
 		echo $local_val."\n";
