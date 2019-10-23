41bc5c9743d2ba9734f206fed0f67c7b80818baa
Mariusz Musik <m@serwer-robelit.local>
2019-10-23 13:56:21 +0200

    Update fronius.php

--- modules/sensors/fronius.php
+++ modules/sensors/fronius.php
@@ -13,8 +13,10 @@ curl_close($ch);
 
 // Will dump a beauty json :3
 var_dump(json_decode($result, true));
-$array[];
-$dayenergy = $array["Body"]["Data"]["DAY_ENERGY"]["Unit"]["Value"];
-echo $dayenergy;
+//$array[];
+//$dayenergy = $array["Body"]["Data"]["DAY_ENERGY"]["Unit"]["Value"];
+$reads = json_decode($result);
+
+echo $reads->DAY_ENERGY->Value;
 
 ?>
