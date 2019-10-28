e4e780173b1cf79420d390dcb5c9e8aea3c29941
Mariusz Musik <m@serwer-robelit.local>
2019-10-28 13:08:15 +0100

    Update status.php

--- status/status.php
+++ status/status.php
@@ -74,6 +74,8 @@ Go to device scan!
 			
 			//JG GROUPS
 			foreach($result_ch_g as $uniqa) {
+				
+				$uniquea[];
 				if(!empty($uniqa['ch_group'])&&$uniqa['ch_group']!='none'&&!in_array($uniqa['ch_group'], $uniquea)) {
 					$uniquea[]=$uniqa['ch_group'];
 					$ch_g=$uniqa['ch_group'];
