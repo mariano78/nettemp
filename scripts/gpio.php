c7c54f39ab566a66f2cb54bd5d10f40ecdea1d33
Mariusz Musik <m@serwer-robelit.local>
2019-11-20 12:34:23 +0100

    Update gpio.php

--- scripts/gpio.php
+++ scripts/gpio.php
@@ -15,29 +15,63 @@ $ROOT=dirname(dirname(__FILE__));
 echo $ROOT;
 $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 
+$action = '';
+
 
 $rows = $db->query("SELECT status, ip FROM gpio WHERE rom = '$rom' AND gpio = '$gpio'");
 $row = $rows->fetchAll();
 
 foreach ($row as $a) {
 	
-	$state = $a['status'];
+	$state = strtolower($a['status']);
 	$ip = $a['ip'];
 }
 
-
-
-echo $status;
+echo $state;
 echo $ip;
 
+function scurl($ip,$gpio,$action){
+	
+	$url = 'http://'.$ip.'/control?cmd=GPIO,'.$gpio.','.$action;
+	
+			$ch = curl_init($url);
+			curl_setopt( $ch, CURLOPT_POST, 1);
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, $data);
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+			 
+			$response = curl_exec( $ch );
+			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			curl_close($ch);
+	
+	
+}
 
 if ($act == 'auto'){
 	
+	if ($state == 'on') {
+		
+		$action = '0';
+		scurl ($ip,$gpio,$action );
+		
+	} else if ($state == 'off') {
+		
+		$action = '1';
+		scurl ($ip,$gpio,$action );
+	}
+	
 }else if ($act == 'on'){
+	
+		$action = '1';
+		scurl ($ip,$gpio,$action );
 	
 }else if ($act == 'off'){
 	
+		$action = '0';
+		scurl ($ip,$gpio,$action );
+	
 }else if ($act == 'status'){
+	
+		echo $state; 
 	
 }
 
