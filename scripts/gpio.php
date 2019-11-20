340594ea3a31b056b7e2332c7bb633ea063d9d6a
Mariusz Musik <m@serwer-robelit.local>
2019-11-20 12:04:01 +0100

    Update gpio.php

--- scripts/gpio.php
+++ scripts/gpio.php
@@ -11,7 +11,24 @@ $gpio=$params['gpio'];
 parse_str($argv[3],$params);
 $act=$params['act'];
 
+$ROOT=dirname(__FILE__)));
+echo $ROOTt;
+$db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 
+
+$state = $db->querySingle("SELECT status FROM gpio WHERE rom = '$rom' AND gpio = '$gpio'");
+
+echo $state;
+
+if ($act == 'auto'){
+	
+}else if ($act == 'on'){
+	
+}else if ($act == 'off'){
+	
+}else if ($act == 'status'){
+	
+}
 
 
 ?>
