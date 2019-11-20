02a0b622e1733f5b963eabc141621926772a8b95
Mariusz Musik <m@serwer-robelit.local>
2019-11-20 12:04:45 +0100

    Update gpio.php

--- scripts/gpio.php
+++ scripts/gpio.php
@@ -11,8 +11,8 @@ $gpio=$params['gpio'];
 parse_str($argv[3],$params);
 $act=$params['act'];
 
-$ROOT=dirname(__FILE__)));
-echo $ROOTt;
+$ROOT=dirname(__FILE__);
+echo $ROOT;
 $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 
 
