9557fcb54c2d508678e2946fb051a72d6ba96058
Mariusz Musik <m@serwer-robelit.local>
2019-11-20 12:44:41 +0100

    Update gpio.php

--- scripts/gpio.php
+++ scripts/gpio.php
@@ -1,6 +1,6 @@
 <?php
 
-var_dump($argv);
+//var_dump($argv);
 
 parse_str($argv[1],$params);
 $rom=$params['rom'];
@@ -12,11 +12,9 @@ parse_str($argv[3],$params);
 $act=$params['act'];
 
 $ROOT=dirname(dirname(__FILE__));
-//echo $ROOT;
 $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
 
 $action = '';
-
 
 $rows = $db->query("SELECT status, ip FROM gpio WHERE rom = '$rom' AND gpio = '$gpio'");
 $row = $rows->fetchAll();
@@ -27,20 +25,13 @@ foreach ($row as $a) {
 	$ip = $a['ip'];
 }
 
-//echo $state;
-//echo $ip;
-
 function scurl($ip,$gpio,$action){
 	
 	$url = 'http://'.$ip.'/control?cmd=GPIO,'.$gpio.','.$action;
 	
 			$ch = curl_init($url);
 			curl_setopt( $ch, CURLOPT_POST, 1);
-			//curl_setopt( $ch, CURLOPT_POSTFIELDS, $data);
-			//curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-			 
 			curl_exec( $ch );
-			//$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 			curl_close($ch);
 	
 	
