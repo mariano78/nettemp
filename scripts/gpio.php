e6c2bd93d6c49ac9e76e85f191fc436321bb7d32
Mariusz Musik <m@serwer-robelit.local>
2019-11-20 12:40:38 +0100

    Update gpio.php

--- scripts/gpio.php
+++ scripts/gpio.php
@@ -36,11 +36,11 @@ function scurl($ip,$gpio,$action){
 	
 			$ch = curl_init($url);
 			curl_setopt( $ch, CURLOPT_POST, 1);
-			curl_setopt( $ch, CURLOPT_POSTFIELDS, $data);
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+			//curl_setopt( $ch, CURLOPT_POSTFIELDS, $data);
+			//curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 			 
-			$response = curl_exec( $ch );
-			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			//$response = curl_exec( $ch );
+			//$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 			curl_close($ch);
 	
 	
