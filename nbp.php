df89e73f8b577250ca20733b8604887b83c88ba6
mariano78 <mariano78@interia.pl>
2020-02-20 13:41:47 +0100

    Update nbp.php

--- nbp.php
+++ nbp.php
@@ -12,11 +12,13 @@ try {
 	$obj = json_decode($json,true);
 	//var_dump($obj);
 	
-	foreach ($obj as $value) {
-		
-		$value2 = $value['no'];
-		echo $value2;
-	}
+
+	  foreach($obj['rates'] as $key=>$val){// this can be ommited if only 0 index is there after 
+//league and $data['league'][0]['events'] can be used in below foreach instead of $val['events'].
+        echo $val['effectiveDate']." - Kurs - ".$val['mid']."\n"; 
+    }  
+    
+    //effectiveDate
 	
 	
 	} catch (Exception $e) {
@@ -25,4 +27,3 @@ try {
     exit;
 }
 ?>
-
