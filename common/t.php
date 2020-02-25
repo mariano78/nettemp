4c946f4aff3024c41e54a02ce9bcbca43bd5561a
mariano78 <mariano78@interia.pl>
2020-02-25 12:45:44 +0100

    Update t.php

--- common/t.php
+++ common/t.php
@@ -1,7 +1,7 @@
 <?php
 
-require "common/influx_sender.php";
-sendInflux(22, 100, jakisrom, nazwa, temp);
+require "/var/www/nettemp/common/influx_sender.php";
+sendInflux(22, 100, "jakisrom", "nazwa8", "temp");
 
 
 
