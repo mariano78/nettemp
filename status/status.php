5e840b9265a181f5f96975077bfa542d8ddf2431
mariano78 <mariano78@interia.pl>
2020-02-29 12:35:02 +0100

    Update status.php

--- status/status.php
+++ status/status.php
@@ -56,6 +56,7 @@ function numberFormatPrecision($number, $separator, $precision)
 	//if ($precision == '0') { $response = substr($numberParts[0],0,-1);};
 	
     $response = $numberParts[0];
+    if(count($numberParts)>1 && $precision != '0'){
         $response .= $separator;
         $response .= substr($numberParts[1], 0, $precision);
     } else {
