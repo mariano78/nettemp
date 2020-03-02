06bb7826ec76eb6668c5e5448e8a7e6814481456
mariano78 <mariano78@interia.pl>
2020-03-02 08:40:27 +0100

    Update status.php

--- status/status.php
+++ status/status.php
@@ -50,20 +50,7 @@ Go to device scan!
 </div>
 <?php
 }
-function numberFormatPrecision($number, $separator, $precision)
-{
-    $numberParts = explode($separator, $number);
-	//if ($precision == '0') { $response = substr($numberParts[0],0,-1);};
-	
-    $response = $numberParts[0];
-    if(count($numberParts)>1 && $precision != 0){
-        $response .= $separator;
-        $response .= substr($numberParts[1], 0, $precision);
-    } else {
-		$response = $numberParts[0];
-	}
-    return $response;
-}
+
 //Modules ORDER
 
  $morder = $db->query("SELECT * FROM statusorder ORDER BY position ASC") or header("Location: html/errors/db_error.php");
