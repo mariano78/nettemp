c9db6e0f1d332fae3c274c2bdff8a1782369f18e
mariano78 <mariano78@interia.pl>
2020-03-25 10:30:56 +0100

    links

---
+++ modules/settings/ownlink.php
@@ -0,0 +1,87 @@
+<?php
+    $csave = isset($_POST['csave']) ? $_POST['csave'] : '';
+    $cip = isset($_POST['cip']) ? $_POST['cip'] : '';
+	$cport = isset($_POST['cport']) ? $_POST['cport'] : '';
+    $ckey = isset($_POST['ckey']) ? $_POST['ckey'] : '';
+    if ($csave == "csave"){
+    $db = new PDO('sqlite:dbf/nettemp.db');
+    $db->exec("UPDATE nt_settings SET value='$cip' WHERE option='client_ip'") or die ($db->lastErrorMsg());
+    $db->exec("UPDATE nt_settings SET value='$ckey' WHERE option='client_key'") or die ($db->lastErrorMsg());
+	$db->exec("UPDATE nt_settings SET value='$cport' WHERE option='client_port'") or die ($db->lastErrorMsg());
+    header("location: " . $_SERVER['REQUEST_URI']);
+    exit();
+    }
+
+ 
+
+$root=$_SERVER["DOCUMENT_ROOT"];
+$db = new PDO("sqlite:$root/dbf/nettemp.db");
+
+
+	$sth = $db->prepare("SELECT * FROM ownlinks  ORDER BY id ASC");
+    $sth->execute();
+    $result = $sth->fetchAll(); 
+    $numsen = count($result);
+
+
+ 
+?>
+
+
+<div class="panel panel-default">
+<div class="panel-heading">
+<h3 class="panel-title">Own links in menu</h3>
+</div>
+<div class="panel-body">
+<div class="table-responsive">
+<table class="table table-hover table-condensed small">
+
+<thead>
+	<tr>
+		<th>Name</th>
+		<th>Link</th>
+		<th>on/off</th>
+	</tr>
+</thead>
+
+<tbody>
+
+
+
+
+</tbody>
+</table>
+</div>
+
+
+<?php
+
+foreach ($result as $a) {
+	
+	
+	
+	
+	
+	
+}
+
+
+
+?>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<?php
+
+
+
+
--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -298,7 +298,7 @@ $updates['2020-03-04 10:00:50'][]="ALTER TABLE inverters ADD user TEXT";
 $updates['2020-03-04 10:00:51'][]="ALTER TABLE inverters ADD pass TEXT";
 
 //Own links in menu
-$updates['2020-03-25 09:39:40'][]="CREATE TABLE ownlinks (id INTEGER PRIMARY KEY, name TEXT, link TEXT)";
+$updates['2020-03-25 09:39:40'][]="CREATE TABLE ownlinks (id INTEGER PRIMARY KEY, name TEXT, link TEXT, onoff TEXT)";
 $updates['2020-03-25 09:40:08'][]="INSERT INTO nt_settings ('option', 'value') VALUES ('chartsforall','off')";
 
 
