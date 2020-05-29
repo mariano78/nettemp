1b86df679f6b24fffc8192a0766c2d2cdc78006d
mariano78 <mariano78@interia.pl>
2020-05-29 13:43:28 +0200

    Update gpio_day_plan.php

--- modules/gpio/html/gpio_day_plan.php
+++ modules/gpio/html/gpio_day_plan.php
@@ -77,6 +77,7 @@ $active2 = isset($_POST['active2']) ? $_POST['active2'] : '';
 	<td><input type="checkbox" name="fri" value="Fri" /></td>        
 	<td><input type="checkbox" name="sat" value="Sat" /></td>        
 	<td><input type="checkbox" name="sun" value="Sun" /></td>
+	<td><input type="checkbox" name="active2" value="on" /></td>
 	<td><input type="text" name="stime" value="" class="form-control" required="" placeholder="07:00"/></td>
 	<td><input type="text" name="etime" value="" class="form-control" required="" placeholder="19:00"/></td>
 	<input type="hidden" name="add1" value="add2" />
