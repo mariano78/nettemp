856fb6575c019e4a26b0f1f065d4e10775077c35
mariano78 <mariano78@interia.pl>
2020-06-16 12:39:54 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -1,1 +1,12 @@
 <textarea id="java-code"></textarea>
+<script>
+
+ var javaEditor = CodeMirror.fromTextArea(document.getElementById("java-code"), {
+    lineNumbers: true,
+    matchBrackets: true,
+    mode: "text/x-java"
+  });
+  var mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
+  CodeMirror.keyMap.default[(mac ? "Cmd" : "Ctrl") + "-Space"] = "autocomplete";
+  
+ </script>
