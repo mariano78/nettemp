c066e7f2090f4cc6209f0dcafdbf30c038e9ad1f
mariano78 <mariano78@interia.pl>
2020-06-16 12:43:52 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -1,6 +1,6 @@
 <!-- Create a simple CodeMirror instance -->
-<link rel="stylesheet" href="codemirror/codemirror.css">
-<script src="codemirror/codemirror.js"></script>
+<link rel="stylesheet" href="/var/www/nettemp/codemirror/codemirror.css">
+<script src="/var/www/nettemp/codemirror/codemirror.js"></script>
 <script>
   var editor = CodeMirror.fromTextArea(myTextarea, {
     lineNumbers: true
