451dafe563f0e18f1ad4c165d20efa3bece60404
mariano78 <mariano78@interia.pl>
2020-06-16 13:01:16 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -4,10 +4,31 @@
 <div class="panel-heading"><h3 class="panel-title">Scripts</h3></div>
 <div class="panel-body">
 <div class="grid">
+
+
+<form style="width:500px;">
+            <textarea id="code" name="code">
+alert("HI");
+//says HII
+            </textarea>
+        </form>
+		
+		
+		
 	
 	<script>
-		var myCodeMirror = CodeMirror(document.body);
-	</script>
+            window.onload = function() {
+                window.editor = CodeMirror.fromTextArea(code, {
+                    mode: "javascript",
+                    lineNumbers: true,
+                    lineWrapping: true,
+                    foldGutter: {
+                        rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.brace, CodeMirror.fold.comment)
+                    },
+                    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+                });
+            };
+        </script>
 
 
 </div>
