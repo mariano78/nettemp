b3fb3c09f0505ba2130a16a7d32254d055184a37
mariano78 <mariano78@interia.pl>
2020-06-16 12:06:24 +0200

    Update variables.php

--- modules/settings/variables.php
+++ modules/settings/variables.php
@@ -1,27 +1,29 @@
 <?php
-    $save = isset($_POST['save']) ? $_POST['save'] : '';
-    $type = isset($_POST['type']) ? $_POST['type'] : '';
-    $unit = isset($_POST['unit']) ? $_POST['unit'] : '';
-    $unit2 = isset($_POST['unit2']) ? $_POST['unit2'] : '';
-    $title = isset($_POST['title']) ? $_POST['title'] : '';
-	$ico = isset($_POST['ico']) ? $_POST['ico'] : '';
-    
-	$add = isset($_POST['add']) ? $_POST['add'] : '';
-	$min = isset($_POST['min']) ? $_POST['min'] : '';
-	$max = isset($_POST['max']) ? $_POST['max'] : '';
-	$value1 = isset($_POST['value1']) ? $_POST['value1'] : '';
-	$value2 = isset($_POST['value2']) ? $_POST['value2'] : '';
-	$value3 = isset($_POST['value3']) ? $_POST['value3'] : '';
 
-
-
-   	 
-    if ($save == 'save1'){
+		
+	
+	
+	$id = isset($_POST['id']) ? $_POST['id'] : '';
+	$mod_name = isset($_POST['mod_name']) ? $_POST['mod_name'] : '';
+	$mod_val = isset($_POST['mod_val']) ? $_POST['mod_val'] : '';
+	$value = isset($_POST['value']) ? $_POST['value'] : '';
+	$name = isset($_POST['name']) ? $_POST['name'] : '';
+	
+    if ($mod_val == 'mod_val'){
     $db = new PDO('sqlite:dbf/nettemp.db');
-    $db->exec("UPDATE types SET title='$title',unit2='$unit2',unit='$unit',type='$type',ico='$ico',min='$min',max='$max',value1='$value1',value2='$value2',value3='$value3' WHERE id='$save_id'") or header("Location: html/errors/db_error.php");
+    $db->exec("UPDATE ovariables SET value='$value' WHERE id='$id'") or header("Location: html/errors/db_error.php");
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
     }
+	
+	if ($mod_name == 'mod_name'){
+    $db = new PDO('sqlite:dbf/nettemp.db');
+    $db->exec("UPDATE ovariables SET name='$name' WHERE id='$id'") or header("Location: html/errors/db_error.php");
+    header("location: " . $_SERVER['REQUEST_URI']);
+    exit();
+    }
+	
+	
     if ($add == 'add1'){
     $db = new PDO('sqlite:dbf/nettemp.db');
     $db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title, min, max, value1, value2 ,value3) VALUES ('$type','$unit','$unit2','$ico','$title','$min','$max','$value1','$value2','$value3')") or header("Location: html/errors/db_error.php");
@@ -33,7 +35,7 @@
 	$del_id = isset($_POST['del_id']) ? $_POST['del_id'] : '';
     if ($del == 'del1'){
     $db = new PDO('sqlite:dbf/nettemp.db');
-	 $db->exec("DELETE FROM ovariables WHERE id='$del_id'") or die ("cannot insert to DB");
+	 $db->exec("DELETE FROM ovariables WHERE id='$del_id'") or die ("Location: html/errors/db_error.php");
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
     }
@@ -55,6 +57,7 @@ $row = $rows->fetchAll();
 <th>Name</th>
 <th>Value</th>
 <th>Delete</th>
+<th></th>
 </tr>
 </thead>
 
@@ -65,7 +68,7 @@ $row = $rows->fetchAll();
 	 
 	<td class="col-md-2">
 		<form action="" method="post" style="display:inline!important;">
-			<input type="text" name="type" size="5" maxlength="30" value="<?php echo $a['name']; ?>" class="form-control input-sm"/>
+			<input type="text" name="name" size="5" maxlength="30" value="<?php echo $a['name']; ?>" class="form-control input-sm"/>
 			<input type="hidden" name="id" value="<?php echo $a['id']; ?>" />
 			<input type="hidden" name="mod_name" value="mod_name" />
 			<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
@@ -74,7 +77,7 @@ $row = $rows->fetchAll();
     
 	<td class="col-md-2">
 		<form action="" method="post" style="display:inline!important;">
-			<input type="text" name="unit" size="5" maxlength="30" value="<?php echo $a['value']; ?>" class="form-control input-sm"/>
+			<input type="text" name="value" size="5" maxlength="30" value="<?php echo $a['value']; ?>" class="form-control input-sm"/>
 			<input type="hidden" name="id" value="<?php echo $a['id']; ?>" />
 			<input type="hidden" name="mod_val" value="mod_val" />
 			<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
@@ -88,6 +91,10 @@ $row = $rows->fetchAll();
 			<input type="hidden" name="del_id" value="<?php echo $a['id']; ?>" />
 			<input type="hidden" name="del" value="del1"/>
 		</form>
+    </td>
+	
+	<td class="col-md-7">
+		
     </td>
 </tr>
    
