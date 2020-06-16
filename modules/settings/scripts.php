1c94ff97ce0c8a8952e54f8204f7954436e9e194
mariano78 <mariano78@interia.pl>
2020-06-16 12:55:56 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -1,5 +1,16 @@
 <!-- Create a simple CodeMirror instance -->
 
+<div class="panel panel-default">
+<div class="panel-heading"><h3 class="panel-title">Scripts</h3></div>
+<div class="panel-body">
+<div class="grid">
+	
 <script>
   var myCodeMirror = CodeMirror(document.body);
 </script>
+
+
+</div>
+</div>
+</div>
+
