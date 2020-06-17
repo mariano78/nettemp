ac7744d2f14b0821ec165458c0626ee7c0b501d5
mariano78 <mariano78@interia.pl>
2020-06-17 13:27:21 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -20,18 +20,11 @@ print_r($a);
 foreach ($a as $rfile){
 	
 	echo $rfile;
-	?> <input type="button"  id = "show_content" value = " <?php echo $rfile; ?> " onchange="loadfile(this)">aaa</input>
+	?> <input type="button"  id = "show_content" value = " <?php echo "$root/tmp/"."$rfile"; ?> " onchange="loadfile(this)">aaa</input>
 <?php
 }
 
 ?>
-
-<script type="text/javascript">
-  $("#show_content").click(function(){
-    $("#code").load("$root/tmp/zawor.php");
-  });
-</script>
-
 
 
 <script>
