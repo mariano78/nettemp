dcae57fd8457b17a6bf05b47a40fb9959b950509
mariano78 <mariano78@interia.pl>
2020-06-17 14:15:57 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -32,7 +32,7 @@ foreach ($a as $rfile){
 
 $(document).ready(function() {
     $("#show").click(function() {
-        $(".div1").load('/var/www/nettemp/tmp/zawor.php', function(){
+        $("#div1").load('/var/www/nettemp/tmp/zawor.php', function(){
             alert("Done Loading");
         });
    }); 
