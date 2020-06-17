b02c1e68c47a0a0a7d39ebbd5228eb2b091c4546
mariano78 <mariano78@interia.pl>
2020-06-17 12:48:34 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -15,12 +15,8 @@ $dir = "$root/tmp/";
 
 // Sort in ascending order - this is default
 $a = scandir($dir);
-
-// Sort in descending order
-$b = scandir($dir,1);
-
 print_r($a);
-print_r($b);
+
 ?>
 
 
