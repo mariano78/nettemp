af602398393325d73dab9f779b370cf5adfa7365
mariano78 <mariano78@interia.pl>
2020-06-19 09:06:17 +0200

    Update scripts.php

--- modules/settings/scripts.php
+++ modules/settings/scripts.php
@@ -34,6 +34,45 @@ $(document).ready(function() {
     $("#show").click(function() {
         $("#code").load("/tmp/zawor.php", function(response, status, xhr){
 			
+			// first file selected by user
+	var file = "/tmp/zawor.php";
+	
+	//document.querySelector("#file-input").files[0];
+
+	// perform validation on file type & size if required
+
+	// read the file
+	var reader = new FileReader();
+
+	// file reading started
+	reader.addEventListener('loadstart', function() {
+	    console.log('File reading started');
+	});
+
+	// file reading finished successfully
+	reader.addEventListener('load', function(e) {
+	   // contents of file in variable     
+	    var text = e.target.result;
+
+	    console.log(text);
+	});
+
+	// file reading failed
+	reader.addEventListener('error', function() {
+	    alert('Error : Failed to read file');
+	});
+
+	// file read progress 
+	reader.addEventListener('progress', function(e) {
+	    if(e.lengthComputable == true) {
+	    	var percent_read = Math.floor((e.loaded/e.total)*100);
+	    	console.log(percent_read + '% read');
+	    }
+	});
+
+	// read as text file
+	reader.readAsText(file);
+});
 
 			
 			if ( status == "error" ) {
