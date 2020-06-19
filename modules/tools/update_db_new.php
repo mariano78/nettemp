a91403f18e1ceaaf9c37fbccb78dd190a6e15a79
mariano78 <mariano78@interia.pl>
2020-06-19 11:22:48 +0200

    Update update_db_new.php

--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -313,8 +313,6 @@ $updates['2020-05-27 09:00:51'][]="ALTER TABLE gpio ADD sprinkler_trig TEXT";
 
 //Own variables
 $updates['2020-05-29 11:39:40'][]="CREATE TABLE ovariables (id INTEGER PRIMARY KEY, name TEXT, value TEXT )";
-$updates['2020-05-29 11:40:08'][]="INSERT INTO ovariables ('name', 'value') VALUES ('deszcz24','0.3')";
-$updates['2020-05-29 11:40:10'][]="INSERT INTO ovariables ('name', 'value') VALUES ('deszcz48','1.3')";
 
 //day-plan active
 $updates['2020-05-29 11:41:10'][]="ALTER TABLE day_plan ADD active2 TEXT";
