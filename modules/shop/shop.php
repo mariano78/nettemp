d4d05e84fc2a926189864e963b6d83604f9c7647
Mariusz Musik <m@serwer-robelit.local>
2020-11-11 13:48:52 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -2,7 +2,7 @@
 
 // Connects to the MYDB database described in tnsnames.ora file,
 // One example tnsnames.ora entry for MYDB could be:
-   MYDB = '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.18.240)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = oracp1250)))';
+   $MYDB = '(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.18.240)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = oracp1250)))';
 
 $conn = oci_connect('erp', 'erp', 'MYDB');
 if (!$conn) {
