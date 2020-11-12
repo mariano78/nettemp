e60b23336b5e6c071fae1fe0499fb342b757f6fb
Mariusz Musik <m@serwer-robelit.local>
2020-11-12 12:33:48 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -1,10 +1,34 @@
 <?php
-phpinfo();
-// Connects to the MYDB database described in tnsnames.ora file,
-// One example tnsnames.ora entry for MYDB could be:
-   MYDB = (DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.18.240)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = oracp1250)));
+// phpinfo();
 
-$conn = oci_connect('erp', 'erp', 'MYDB');
+<?php
+if(!empty($_SERVER["DOCUMENT_ROOT"])){
+    $root=$_SERVER["DOCUMENT_ROOT"];
+}else{
+    $root=__DIR__;
+    for($i=0;$i<5;$i++){
+        $root = file_exists($root.'/dbf/nettemp.db') ? $root : dirname($root) ;
+    }
+}
+if(!isset($db)){
+    $db = new PDO("sqlite:$root/dbf/nettemp.db");
+}
+$sth = $db->query("SELECT * FROM shop");
+$sth->execute();
+$result = $sth->fetchAll();
+foreach ($result as $a) {
+	if($a['option']=='user') {
+		$user=$a['value'];
+	}
+	if($a['option']=='pass') {
+		$pass=$a['value'];
+	}
+	if($a['option']=='database') {
+		$database=$a['value'];
+	}
+}	
+
+$conn = oci_connect('$user', '$pass', '$database');
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
