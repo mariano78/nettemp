62588bb2cd9519c1205a7f48b1e85b0374de9dd2
Mariusz Musik <m@serwer-robelit.local>
2020-11-13 12:35:09 +0100

    sad

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -45,27 +45,89 @@ while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
     echo "</tr>\n";
 }
 echo "</table>\n";
-
-use DreamCommerce\ShopAppstoreLib\Client;
-use DreamCommerce\ShopAppstoreLib\Exception\ClientException;
-use DreamCommerce\ShopAppstoreLib\Exception\ResourceException;
-
-
+$config = array(
+      'debug' => false,
+      'logFile' => "logs/application.log",
+      'timezone' => 'Europe/Warsaw',
+      'php' => array(
+          'display_errors' => 'off'
+      )
+  );
  
- try {
-    $client = \DreamCommerce\ShopAppstoreLib\Client::factory(
-       \DreamCommerce\ShopAppstoreLib\Client::ADAPTER_BASIC_AUTH,
-       array(
-           'entrypoint'=>'https://sklep475200.shoparena.pl/',
-           'username' => 'usr',
-           'password' => 'pass'
-       )
-    );
-
-    $resource = new \DreamCommerce\ShopAppstoreLib\Resource\Producer($client);
-    var_dump($resource->get());
-} catch(DreamCommerce\ShopAppstoreLib\Exception\Exception $ex) {
-    die($ex->getMessage());
+  ini_set('default_charset', 'utf-8');
+ 
+  // tutaj UWAGA - scieżka src to przykładowa ścieżka gdzie znajdują się pliki z sdk czyli ten plik znajduje się w katalogu głównym a w podkatalogu src znajdują się odpowidnie elementy sdk (katalogi DreamCommerce i Psr)
+  spl_autoload_register(function($class){
+      $class = str_replace('\\', '/', $class);
+      require 'src/'.$class.'.php';
+  });
+ 
+  date_default_timezone_set($config['timezone']);
+  ini_set('display_errors', $config['php']['display_errors']);
+ 
+  $debug = false;
+ 
+  if(isset($config['debug'])){
+      if($config['debug']){
+          $debug = true;
+      }
+  }
+ 
+  if(getenv('DREAMCOMMERCE_DEBUG')){
+      $debug = true;
+  }
+ 
+  define("DREAMCOMMERCE_DEBUG", $debug);
+ 
+  $logFile = "php://stdout";
+ 
+  if(isset($config['logFile'])){
+      if($config['logFile']){
+          $logFile = $config['logFile'];
+      }else{
+          $config['logFile'] = false;
+      }
+  }
+ 
+  define("DREAMCOMMERCE_LOG_FILE", $logFile);
+ 
+  try{
+      $client = \DreamCommerce\ShopAppstoreLib\Client::factory(
+         \DreamCommerce\ShopAppstoreLib\Client::ADAPTER_BASIC_AUTH,
+         array(
+             'entrypoint'=>'https://sklep475200.shoparena.pl/',
+             'username'=>'mmapi',
+             'password'=>'Ala1Ala2!!'
+         )
+      );
+ 
+$resource = new DreamCommerce\ShopAppstoreLib\Resource\Product($client);
+ 
+// Poberanie 1 strony
+$currentPage = 1;
+$result = $resource->page($currentPage)->limit(50)->get();
+ 
+foreach($result as $product){
+    // Pętla po wszystich pierwszych 50 produktach - usuwamy każdy z nich po kolei
+    $deleteResult = $resource->delete($product["product_id"]);
 }
+ 
+$currentPage++;
+ 
+// Pobranie kolejnych stron jeśli istnieją
+while($currentPage <= $result->getPageCount()){
+  // Pobieramy produkty z kolejnej strony
+  $result = $resource->page($currentPage)->limit(50)->get();
+ 
+  foreach($result as $product){
+      // Pętla po 50 produktach z kolejnych stron - usuwamy każdy z nich po kolei
+      $deleteResult = $resource->delete($product["product_id"]);
+  }
+ 
+  $currentPage++;
+}
+  }catch(DreamCommerce\ShopAppstoreLib\Exception\Exception $ex) {
+      die($ex->getMessage());
+  }
 
 ?>
