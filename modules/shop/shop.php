acdb1c450065a31343535f4dd1397fefae59b4ef
mariano78 <mariano78@interia.pl>
2020-11-16 15:13:33 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -21,13 +21,14 @@ oci_execute($stid);
 
 while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
 	
-	foreach ($row as $item) {
-        //echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+	
 		$kod = $row['TO_KOD'];
-	echo $kod."<br>";
-    }
+	
     
 }
+
+oci_free_statement($stid);
+oci_close($conn);
 	
 
  
