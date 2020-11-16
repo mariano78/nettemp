420b2f756f0fa73b2cd56dd20abe721b6f0570f1
mariano78 <mariano78@interia.pl>
2020-11-16 14:02:45 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -22,6 +22,8 @@ while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
     echo "<tr>\n";
     foreach ($row as $item) {
         echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+		echo $item['TO_KOD'];
+		echo $item['MAGAZYN';
     }
     echo "</tr>\n";
 }
