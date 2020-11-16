e1d9a84dd2aed17d04b0ebb97a0763062d4304d7
mariano78 <mariano78@interia.pl>
2020-11-16 20:00:46 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -149,6 +149,7 @@ $time_post = microtime(true);
 $exec_time = $time_post - $time_pre;
   
 $db->exec("UPDATE shop SET value='$exec_time' WHERE option='etime'");
+echo date("H:i:s",$exec_time);
  
 
 ?>
