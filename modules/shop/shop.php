f411bbdf03441d981444fedb2cba37aef25a712e
mariano78 <mariano78@interia.pl>
2020-11-16 16:41:54 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -36,6 +36,7 @@ while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
     foreach($result as $r){
 		
 		$kod_shop = $r->stock->code;
+		echo '--'.$kod_shop.'--';
 		
 		if ($kod == $kod_shop) {
 			
