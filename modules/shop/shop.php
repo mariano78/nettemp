78246a4fccbd4a494545e806a34f8b1fa9d57c75
mariano78 <mariano78@interia.pl>
2020-11-16 15:42:56 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -26,6 +26,7 @@ while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
 		
 	$resource = new DreamCommerce\ShopAppstoreLib\Resource\Product($client);
     $result = $resource->get();
+	var_dump($result);
 
     foreach($result as $r){
         printf("#%d - %s\n", $r->product_id, $r->translations->pl_PL->name, $r->stock->code, $r->stock->ean);
