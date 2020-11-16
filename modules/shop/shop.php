5b03d01dece91683a7e7b9a90ce10ef76da44350
mariano78 <mariano78@interia.pl>
2020-11-16 15:21:36 +0100

    Update shop.php

--- modules/shop/shop.php
+++ modules/shop/shop.php
@@ -28,7 +28,7 @@ while (($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
     $result = $resource->get();
 
     foreach($result as $r){
-        printf("#%d - %s\n", $r->product_id, $r->translations->pl_PL->name, $r->stack.code);
+        printf("#%d - %s\n", $r->product_id, $r->translations->pl_PL->name, $r->stock.code);
     }
 	
     
