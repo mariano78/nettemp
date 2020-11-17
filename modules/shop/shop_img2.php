17b4cf6543ff61d60c25f1b96ef29e3eccb6233a
mariano78 <mariano78@interia.pl>
2020-11-17 15:21:18 +0100

    Update shop_img2.php

--- modules/shop/shop_img2.php
+++ modules/shop/shop_img2.php
@@ -11,16 +11,20 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 // Dołączam ustawienia Oracle i sdk shoper
 include("$root/modules/shop/shop_settings.php");
 
-$url="https://robelit.pl/wp-content/uploads/2020/11/5905725026302/";
-//https://robelit.pl/wp-content/uploads/2020/04/PHOTO-2020-04-09-00-12-33_-600x600.jpg
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
+
+function curl_get_contents($url)
+{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 $html = curl_exec($ch);
-curl_close($ch);
-
-echo $html;
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+$url="https://robelit.pl/wp-content/uploads/2020/11/5905725026302/";
+curl_get_contents($url);
 
 
 
