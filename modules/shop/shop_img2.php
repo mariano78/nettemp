5885e602aa6fa802b4ef277cf9a30fe5f0aef91f
mariano78 <mariano78@interia.pl>
2020-11-17 15:05:11 +0100

    Update shop_img2.php

--- modules/shop/shop_img2.php
+++ modules/shop/shop_img2.php
@@ -11,11 +11,18 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 // Dołączam ustawienia Oracle i sdk shoper
 include("$root/modules/shop/shop_settings.php");
 
-$url = '//serwer/Handlowka/_Nowy_Sklep/Produkty/5905725026302/';
-$no_html = strip_tags(file_get_contents($url));
-$arr = explode('Parent Directory', $no_html);
-$files = trim($arr[1]);
-$files = explode("\n ", $files);
-var_dump($files);
+$context = stream_context_create(
+    array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+);
+
+echo file_get_contents("http://www.robelit.pl/5905725026302", false, $context);
+
+
+
+
 
 ?>
