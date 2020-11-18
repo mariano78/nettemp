5cbb3c9cebf9fcc99968d8d05e070ce34cf7416b
mariano78 <mariano78@interia.pl>
2020-11-18 10:29:14 +0100

    Update shop_img2.php

--- modules/shop/shop_img2.php
+++ modules/shop/shop_img2.php
@@ -42,8 +42,9 @@ if ($count != '0') {
 	 foreach($result as $r){
         printf("#%d - %s\n", $r->product_id, $r->translations->pl_PL->name);
 		$ean = $r->stock->ean;
-		$file_list = ftp_nlist($conn, $ean);
+		$file_list = ftp_nlist($conn, 'aaa');
 		var_dump($file_list);
+		
 		foreach ($file_list as $file)
 		{
 		  
