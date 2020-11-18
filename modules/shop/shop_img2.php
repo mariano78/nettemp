84c5ed73bd634aeca0dec61e62a34d8a15b9e518
mariano78 <mariano78@interia.pl>
2020-11-18 15:02:55 +0100

    Update shop_img2.php

--- modules/shop/shop_img2.php
+++ modules/shop/shop_img2.php
@@ -17,156 +17,170 @@ $www_serwer = "http://robelit.pl/shopimg/";
 $resource = new DreamCommerce\ShopAppstoreLib\Resource\Product($client);
 	//filtry
 	//$resource->filters(['stock.code'=> ['LIKE'=> $kod]]);
-    
+    $result = $resource->page($currentPage)->limit(50)->get();
 	
 	$currentPage = 1;
-	$result = $resource->page($currentPage)->limit(50)->get();
+	$currentProd = 1;
+	$pages = $result->pages;
 	
-	//$result = $resource->get();
+while($currentPage <= $pages ){
+	  
+	  $result = $resource->page($currentPage)->limit(50)->get();
 	
-	//var_dump($result);
-	$count = $result->count;
-	echo "count_".$count."\n";
+			//$result = $resource->get();
+	
+				//var_dump($result);
+				$count = $result->count;
+				//echo "count_".$count."\n";
 
-if ($count != '0') {
-	
-	$ftp_host = "robelit.home.pl";
-	$ftp_user = "shoper@robelit.pl";
-	$ftp_password = "Ala1Ala2";
+			if ($count != '0') {
+				
+				$ftp_host = "robelit.home.pl";
+				$ftp_user = "shoper@robelit.pl";
+				$ftp_password = "Ala1Ala2";
 
-	//Connect
-	echo "\n";
-	echo "Connecting to $ftp_host via FTP... \n";
-	$conn = ftp_connect($ftp_host);
-	$login = ftp_login($conn, $ftp_user, $ftp_password);
-	$mode = ftp_pasv($conn, TRUE); //Enable PASV ( Note: must be done after ftp_login() )
-	if ((!$conn) || (!$login) || (!$mode)) { //Login OK ?
-	   die("FTP connection has failed ! \n");
-	}
-	echo "Login Ok. \n";
-	
-	// dla każdego produktu w shoperze 
-	 foreach($result as $r){
-        printf("#%d - %s\n", $r->product_id, $r->translations->pl_PL->name);
-		$ean = $r->stock->ean;
-		$id = $r->product_id;
-		$kod = $r->code;
-		$prod_name = $r->translations->pl_PL->name;
-		$file_list = ftp_nlist($conn, $ean);
-		$filteredFiles = preg_grep( '/\.jpg$/i', $file_list );
-		sort($filteredFiles);
-		
-		if($file_list) {
-			
-			//1. sprawdzam czy sa zdjecia, jesli sa usuwam
-			$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
-			//filtry
-			$resource->filters(['product_id'=> ['LIKE'=> $id]]);
-			$result_img = $resource->get();
-			
-			$count_img = $result_img->count;
-			echo "count_img_".$count_img."\n";
-			
-			//usuwam zdjecia
-			if ($count_img != 0){
+				//Connect
+				echo "\n";
+				echo "Connecting to $ftp_host via FTP... \n";
+				$conn = ftp_connect($ftp_host);
+				$login = ftp_login($conn, $ftp_user, $ftp_password);
+				$mode = ftp_pasv($conn, TRUE); //Enable PASV ( Note: must be done after ftp_login() )
+				if ((!$conn) || (!$login) || (!$mode)) { //Login OK ?
+				   die("FTP connection has failed ! \n");
+				}
+				echo "Login Ok. \n";
+				
+				// dla każdego produktu w shoperze 
+				 foreach($result as $r){
+					printf("#%d - %s\n", $r->product_id, $r->translations->pl_PL->name);
+					$ean = $r->stock->ean;
+					$id = $r->product_id;
+					$kod = $r->code;
+					$prod_name = $r->translations->pl_PL->name;
+					$file_list = ftp_nlist($conn, $ean);
+					$filteredFiles = preg_grep( '/\.jpg$/i', $file_list );
+					sort($filteredFiles);
+					
+					if($file_list) {
+						
+						//1. sprawdzam czy sa zdjecia, jesli sa usuwam
+						$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
+						//filtry
+						$resource->filters(['product_id'=> ['LIKE'=> $id]]);
+						$result_img = $resource->get();
+						
+						$count_img = $result_img->count;
+						//echo "count_img_".$count_img."\n";
+						
+						//usuwam zdjecia
+						if ($count_img != 0){
 
-				foreach($result_img as $r_img){
-					$gfx_id = $r_img->gfx_id;
-					$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
-					$result_del_img = $resource->delete($gfx_id);
-					if($result_del_img){
-						echo "Usunięto zdjęcie dla produktu ", $kod." \n";
-					}
+							foreach($result_img as $r_img){
+								$gfx_id = $r_img->gfx_id;
+								$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
+								$result_del_img = $resource->delete($gfx_id);
+								if($result_del_img){
+									echo "Usunięto zdjęcie dla produktu ", $kod." \n";
+								}
+							}
+						//usuwam zdjecia
+							// dodaje zdjęcia
+							foreach ($filteredFiles as $file)
+							{
+								$ext = substr($file, -4);//sprawdzam rozszerzenie
+								$img_name = substr($file, 0, 13);//sprawdzam rozszerzenie
+								$img_name2 = substr($file, strpos($file, "_") + 1);    
+							  
+								$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
+								$data = array(
+									'product_id' => $id,
+									//'file' => $file,
+									'url' => $www_serwer.$file,
+									'translations' => array(
+										'pl_PL' => array(
+											'name' => $prod_name
+										)
+									)
+								);
+								
+								$idz = $resource->post($data);
+								
+								if($idz){
+									$date = date('H:i:s');
+									echo "Dodano zdjęcie do produktu ", $kod." \n";
+									logs_shop($date, 'Info', "Dodano zdjęcie do produktu ". $kod);
+								}
+							}
+							} else { 
+						
+						
+						// dodaje zdjęcia
+							foreach ($filteredFiles as $file)
+							{
+								$ext = substr($file, -4);//sprawdzam rozszerzenie
+								$img_name = substr($file, 0, 13);//sprawdzam rozszerzenie
+								$img_name2 = substr($file, strpos($file, "_") + 1);    
+							  
+								$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
+								$data = array(
+									'product_id' => $id,
+									//'file' => $file,
+									'url' => $www_serwer.$file,
+									'translations' => array(
+										'pl_PL' => array(
+											'name' => $prod_name
+										)
+									)
+								);
+								
+								$idz = $resource->post($data);
+								
+								if($idz){
+								$date = date('H:i:s');
+								echo"Dodano zdjęcie do produktu ", $kod." \n";
+								logs_shop($date, 'Info', "Dodano zdjęcie do produktu ". $kod);
+								}
+							  
+							}
+						
+						}
+						// dodaje zdjęcia
+						
+						
+						
+						
+						
+						
+						
+					} else {
+						
+						echo "Nie ma folderu FTP dla produktu ", $kod." \n";
+						
+						
+						
+					}// jesli nie ma folderu/plikow zrob else i logi
+					
+					
+					
+					
+					
+					
+					
 				}
-			//usuwam zdjecia
-				// dodaje zdjęcia
-				foreach ($filteredFiles as $file)
-				{
-					$ext = substr($file, -4);//sprawdzam rozszerzenie
-					$img_name = substr($file, 0, 13);//sprawdzam rozszerzenie
-					$img_name2 = substr($file, strpos($file, "_") + 1);    
-				  
-					$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
-					$data = array(
-						'product_id' => $id,
-						//'file' => $file,
-						'url' => $www_serwer.$file,
-						'translations' => array(
-							'pl_PL' => array(
-								'name' => $prod_name
-							)
-						)
-					);
-					
-					$idz = $resource->post($data);
-					
-					if($idz){
-						$date = date('H:i:s');
-						echo "Dodano zdjęcie do produktu ", $kod." \n";
-						logs_shop($date, 'Info', "Dodano zdjęcie do produktu ". $kod);
-					}
-				}
-				} else { 
-			
-			
-			// dodaje zdjęcia
-				foreach ($filteredFiles as $file)
-				{
-					$ext = substr($file, -4);//sprawdzam rozszerzenie
-					$img_name = substr($file, 0, 13);//sprawdzam rozszerzenie
-					$img_name2 = substr($file, strpos($file, "_") + 1);    
-				  
-					$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
-					$data = array(
-						'product_id' => $id,
-						//'file' => $file,
-						'url' => $www_serwer.$file,
-						'translations' => array(
-							'pl_PL' => array(
-								'name' => $prod_name
-							)
-						)
-					);
-					
-					$idz = $resource->post($data);
-					
-					if($idz){
-					$date = date('H:i:s');
-					echo"Dodano zdjęcie do produktu ", $kod." \n";
-					logs_shop($date, 'Info', "Dodano zdjęcie do produktu ". $kod);
-					}
-				  
-				}
-			
+				// dla każdego produktu w shoperze
+				
+				ftp_close($conn); //close ftp
+				
 			}
-			// dodaje zdjęcia
-			
-			
-			
-			
-			
-			
-			
-		} else {
-			
-			echo "Nie ma folderu FTP dla produktu ", $kod." \n";
-			
-			
-			
-		}// jesli nie ma folderu/plikow zrob else i logi
-		
-		
-		
-		
-		
-		
-		
-    }
-	// dla każdego produktu w shoperze
-	
-	ftp_close($conn); //close ftp
-	
+	 
+
+	  $currentPage++;
 }
+//koniec
+	
+	
+	
+	
 
 
 
