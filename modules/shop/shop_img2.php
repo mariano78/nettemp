33756b2c1c7a121e98390cac18da3f8fe3cea2ca
mariano78 <mariano78@interia.pl>
2020-11-18 13:26:42 +0100

    Update shop_img2.php

--- modules/shop/shop_img2.php
+++ modules/shop/shop_img2.php
@@ -76,26 +76,25 @@ if ($count != '0') {
 			// dodaje zdjęcia
 				echo "Brak zdjęc - dodaje nowe\n";
 				var_dump($filteredFiles);
-				
-				$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
-				
+			
 				foreach ($filteredFiles as $file)
 				{
 				  
 					echo $file;
+					$i = 1;
 					
 					$ext = substr($file, -4);//sprawdzam rozszerzenie
 					$img_name = substr($file, 0, 13);//sprawdzam rozszerzenie
 					$img_name2 = substr($file, strpos($file, "_") + 1);    
 				  
-					
+					$resource = new DreamCommerce\ShopAppstoreLib\Resource\ProductImage($client);
 					$data = array(
 						'product_id' => $id,
-						'file' => $file,
+						'file' => $file.$i,
 						'url' => $www_serwer.$file,
 						'translations' => array(
 							'pl_PL' => array(
-								'name' => $prod_name
+								'name' => $prod_name.$i
 							)
 						)
 					);
@@ -104,6 +103,7 @@ if ($count != '0') {
 
 					printf("An object has been added #%d", $id);
 					sleep(1);
+					$i++;
 				  
 				}
 			
