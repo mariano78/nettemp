5922959675f458e1ca43538a285e819e1432607d
mariano78 <mariano78@interia.pl>
2020-11-20 14:14:02 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -12,74 +12,11 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 include("$root/modules/shop/shop_settings.php");
 
 
+$resource = new DreamCommerce\ShopAppstoreLib\Resource\Category($client);
+    $result = $resource->get();
 
+    foreach($result as $r){
+        printf("#%d - %s\n", $r->category_id, $r->translations->pl_PL->name);
+    }
 
 ?>
-
-<div class="panel panel-default">
-<div class="panel-heading">
-<h3 class="panel-title">Shoper ustawienia</h3>
-</div>
-<div class="panel-body">
-
-<form action="" method="post" class="form-horizontal">
-<fieldset>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">DB oracle:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="mip" placeholder="" class="form-control input-md" required="" type="text" value="<?php echo $database; ?>">
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">DB Oracle user:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="mport" placeholder="" class="form-control input-md" required="" type="text" value="<?php echo $user; ?>">
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">DB Oracle password:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="muser" placeholder="" class="form-control input-md"  type="text" value="<?php echo $pass; ?>">
-  </div>
-</div>
-
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Shoper user:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="mpwd" placeholder="" class="form-control input-md"  type="text" value="<?php echo $shopusr; ?>">
-     <input type="hidden" name="msave" value="msave" />
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Shoper password:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="mpwd" placeholder="" class="form-control input-md"  type="text" value="<?php echo $shoppass; ?>">
-     <input type="hidden" name="msave" value="msave" />
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Shoper sklep:</label>  
-  <div class="col-md-4">
-  <input id="textinput" name="mpwd" placeholder="" class="form-control input-md"  type="text" value="<?php echo $shoptest; ?>">
-     <input type="hidden" name="msave" value="msave" />
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="singlebutton"></label>
-  <div class="col-md-4">
-    <button id="singlebutton" name="singlebutton" class="btn btn-xs btn-success">Save</button>
-  </div>
-</div>
-
-</fieldset>
-</form>
-
-</div>
-</div>
