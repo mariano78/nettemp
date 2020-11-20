ad24acb749d498298a267932d9fd36a3ab721e9f
mariano78 <mariano78@interia.pl>
2020-11-20 11:40:28 +0100

    Update shop_dod.php

--- modules/shop/shop_dod.php
+++ modules/shop/shop_dod.php
@@ -47,6 +47,19 @@ $time_pre = microtime(true);
 			$id_tow = $row['ID']; //kod towaru w RB
 			echo "Towar ID - ".$id_tow."\n";
 			
+			$stid = oci_parse($conn, '
+			MERGE INTO SHOPPER_PRODUCTS USING dual ON ( "ID_TOW" = :idtow2 )
+			WHEN MATCHED THEN UPDATE SET "SHOP_TO_DELIVERY"= :devtime
+			WHEN NOT MATCHED THEN INSERT ("ID_TOW","SHOP_TO_DELIVERY") 
+				VALUES ( :idtow2, :devtime )'
+				
+				);
+			oci_bind_by_name($stid, ":idtow2", $id_tow);
+			oci_bind_by_name($stid, ":devtime", $czas_prze);
+			oci_execute($stid);
+			
+			
+			
 		}
 		
 	   
