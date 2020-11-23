e4a1154c663972318d723796dfcf3ad3ba50d314
mariano78 <mariano78@interia.pl>
2020-11-23 14:20:41 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -160,7 +160,7 @@ $stid2 = oci_parse($conn, "$sql");
 					<form action="" method="post"  class="form-inline">
 						<select name="kategoria" class="form-control input-sm small" onchange="this.form.submit()" style="width: 90px;" >
 							<option value="4"  <?php echo $shop_cat == 4 ? 'selected="selected"' : ''; ?>  ><?php echo "kat1" ?></option>
-							<option value="5"  <?php echo $shop_cat == 5 ? 'selected="selected"' : ''; ?>  ><?php echo "kat2" ?></option>
+							<option value="5"  <?php echo $shop_cat == 5 ? 'selected="selected"' : ''; ?>  ><?php echo "kat2kat2kat2kat2kat2kat2kat2kat2kat2kat2kat2kat2" ?></option>
 						</select>
 						<input type="hidden" name="kat_id_tow" value="<?php echo $id_tow; ?>" />
 					</form>
