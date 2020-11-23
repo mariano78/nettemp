f7af260f006eeb4f1df2fa3b70d2c525deedb013
mariano78 <mariano78@interia.pl>
2020-11-23 14:29:18 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -198,10 +198,10 @@ $stid2 = oci_parse($conn, "$sql");
 					</form>
 				</td>
 				
-				<td class="col-md-2">
+				<td class="col-md-1">
 					
 					<form action="" method="post"  class="form-inline">
-						<select name="dostawa_typ" class="form-control input-sm small" onchange="this.form.submit()" style="width: 90px;" >
+						<select name="dostawa_typ" class="form-control input-sm small" onchange="this.form.submit()" style="width: 180px;" >
 							<option value="15"  <?php echo $shop_delivery_typ == 15 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 20" ?></option>
 							<option value="16"  <?php echo $shop_delivery_typ == 16 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 40" ?></option>
 							<option value="17"  <?php echo $shop_delivery_typ == 17 ? 'selected="selected"' : ''; ?>  ><?php echo "Paleta" ?></option>
