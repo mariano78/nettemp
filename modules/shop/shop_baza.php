e69f7b542c40e402c01e7de2a9c19fa750d92d41
mariano78 <mariano78@interia.pl>
2020-11-23 20:36:55 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -154,7 +154,7 @@ $stid2 = oci_parse($conn, "$sql");
 			$in_shop = $row['IN_SHOP'];
 			$shop_cat = $row['CATEGORY'];
 			$shop_delivery = $row['DELIVERY'];
-			$shop_delivery_typ = $row['DELIVERY_TYP'];
+			$shop_delivery_typ = $row['DELIVERY2'];
 			$shop_name = $row['SHOP_NAME'];
 			
 ?>
