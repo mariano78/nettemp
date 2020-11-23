a3dc9b45c4d0b5c4e0ec6223997bff3efb92c1a0
mariano78 <mariano78@interia.pl>
2020-11-23 19:38:58 +0100

    Update shop_dod_do_kat.php

--- modules/shop/shop_dod_do_kat.php
+++ modules/shop/shop_dod_do_kat.php
@@ -36,9 +36,9 @@ $time_pre = microtime(true);
 			$grupa_tow = $row['TO_GRUPA'];
 			echo "Towar ID - ".$id_tow."\n";
 			
-			if ($grupa_tow == 'PPKAN'){
+			if ($grupa_tow == 'PS-PA'){
 				
-				$katt = 63;
+				$katt = 37;
 			
 			
 			
