43c788ca73a198540d603bfb67b3593dd710e47c
mariano78 <mariano78@interia.pl>
2020-11-23 19:43:39 +0100

    Update shop_dod_do_kat.php

--- modules/shop/shop_dod_do_kat.php
+++ modules/shop/shop_dod_do_kat.php
@@ -36,9 +36,9 @@ $time_pre = microtime(true);
 			$grupa_tow = $row['TO_GRUPA'];
 			echo "Towar ID - ".$id_tow."\n";
 			
-			if ($grupa_tow == 'PVCSP'){
+			if ($grupa_tow == 'BITUMG'){
 				
-				$katt = 62;
+				$katt = 47;
 			
 			
 			
