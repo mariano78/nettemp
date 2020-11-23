6d9e2d37ea720a884af2d3a9823e874511a6eb9a
Mariusz Musik <m@serwer-robelit.local>
2020-11-23 10:33:30 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -11,32 +11,6 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 // Dołączam ustawienia Oracle i sdk shoper
 include("$root/modules/shop/shop_settings.php");
 
-$inshop_id_tow=isset($_POST['inshop_id_tow']) ? $_POST['inshop_id_tow'] : '';
-$inshopcheck=isset($_POST['inshopcheck']) ? $_POST['inshopcheck'] : '';
-$inshop1 = isset($_POST['inshop1']) ? $_POST['inshop1'] : '';
-echo $inshop_id_tow;
-echo $inshop1;
-echo $inshopcheck;
-if (!empty($inshop_id_tow) && $inshop1 == "inshop1"){
-    
-	$stid = oci_parse($conn, 'UPDATE SHOPPER_PRODUCTS SET IN_SHOP = :ins WHERE ID_TOW = :isidt');
-	
-	oci_bind_by_name($stid, ":isidt", $in_shop_id_tow);
-	oci_bind_by_name($stid, ":ins", $inshopcheck);
-	oci_execute($stid);
-	oci_free_statement($stid);
-	oci_close($conn);
-	
-	echo "robota";
-	
-	
-    header("location: " . $_SERVER['REQUEST_URI']);
-    exit();
-    }
-
-
-
-
 ?>
 
 
@@ -62,6 +36,32 @@ if (!empty($inshop_id_tow) && $inshop1 == "inshop1"){
 
 <?php
 $total_records = 0;
+
+$inshop_id_tow=isset($_POST['inshop_id_tow']) ? $_POST['inshop_id_tow'] : '';
+$inshopcheck=isset($_POST['inshopcheck']) ? $_POST['inshopcheck'] : '';
+$inshop1 = isset($_POST['inshop1']) ? $_POST['inshop1'] : '';
+echo $inshop_id_tow;
+echo $inshop1;
+echo $inshopcheck;
+if (!empty($inshop_id_tow) && $inshop1 == "inshop1"){
+    
+	$stid = oci_parse($conn, 'UPDATE SHOPPER_PRODUCTS SET IN_SHOP = :ins WHERE ID_TOW = :isidt');
+	
+	oci_bind_by_name($stid, ":isidt", $in_shop_id_tow);
+	oci_bind_by_name($stid, ":ins", $inshopcheck);
+	oci_execute($stid);
+	oci_free_statement($stid);
+	oci_close($conn);
+	
+	echo "robota";
+	
+	
+    header("location: " . $_SERVER['REQUEST_URI']);
+    exit();
+    }
+
+
+
 
 if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; }; 
 $pstop = $paginating;
