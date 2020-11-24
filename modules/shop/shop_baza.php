97a618771014abc4ffeb573cfce1ea369152e084
mariano78 <mariano78@interia.pl>
2020-11-24 15:40:26 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -321,11 +321,11 @@ $stid2 = oci_parse($conn, "$sql");
 					<form action="" method="post"  class="form-inline">
 						<select name="dostawa_typ" class="form-control input-sm small" onchange="this.form.submit()" style="width: 130px;" >
 							<option value="0"  <?php echo $shop_delivery_typ == 0 ? 'selected="selected"' : ''; ?>  ><?php echo "Brak" ?></option>
-							<option value="15"  <?php echo $shop_delivery_typ == 15 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 20" ?></option>
-							<option value="16"  <?php echo $shop_delivery_typ == 16 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 40" ?></option>
-							<option value="17"  <?php echo $shop_delivery_typ == 17 ? 'selected="selected"' : ''; ?>  ><?php echo "Paleta" ?></option>
-							<option value="18"  <?php echo $shop_delivery_typ == 18 ? 'selected="selected"' : ''; ?>  ><?php echo "Własny RB" ?></option>
-							<option value="11"  <?php echo $shop_delivery_typ == 11 ? 'selected="selected"' : ''; ?>  ><?php echo "Osobisty" ?></option>
+							<option value="2"  <?php echo $shop_delivery_typ == 2 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 20" ?></option>
+							<option value="3"  <?php echo $shop_delivery_typ == 3 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 40" ?></option>
+							<option value="4"  <?php echo $shop_delivery_typ == 4 ? 'selected="selected"' : ''; ?>  ><?php echo "Paleta" ?></option>
+							<option value="5"  <?php echo $shop_delivery_typ == 5 ? 'selected="selected"' : ''; ?>  ><?php echo "Własny RB" ?></option>
+							<option value="6"  <?php echo $shop_delivery_typ == 6 ? 'selected="selected"' : ''; ?>  ><?php echo "Osobisty" ?></option>
 						</select>
 						<input type="hidden" name="deliv_id_tow_typ" value="<?php echo $id_tow; ?>" />
 					</form>
