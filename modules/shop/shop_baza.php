76680f574f88e656c8ffdaec6bfd4c8d6cbf60cf
mariano78 <mariano78@interia.pl>
2020-11-24 09:52:48 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -168,11 +168,11 @@ $stid2 = oci_parse($conn, "$sql");
 				<td class="col-md-0"> 
 				
 					<?php if ($rb_stat == 'spr') {
-						echo  "<span class="label label-success"";
+						echo  '<span class="label label-success"';
 						echo $rb_stat;
-						echo "</span>";
+						//echo '</span>';
 						} ?>
-				
+					</span>
 				</td>
 				
 				<td class="col-md-0"> <?php echo $rb_tow_nazwa ?></td>
