93ffe4c668aaa6cfefba7cfdb6b26c08e869028a
mariano78 <mariano78@interia.pl>
2020-11-24 09:39:45 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -301,10 +301,11 @@ $stid2 = oci_parse($conn, "$sql");
 					
 					<form action="" method="post"  class="form-inline">
 						<select name="dostawa" class="form-control input-sm small" onchange="this.form.submit()" style="width: 130px;" >
+							<option value="999"  <?php echo $shop_delivery == 999 ? 'selected="selected"' : ''; ?>  ><?php echo "Brak" ?></option>
 							<option value="2"  <?php echo $shop_delivery == 2 ? 'selected="selected"' : ''; ?>  ><?php echo "2 dni" ?></option>
 							<option value="6"  <?php echo $shop_delivery == 6 ? 'selected="selected"' : ''; ?>  ><?php echo "10 dni" ?></option>
 							<option value="8"  <?php echo $shop_delivery == 8 ? 'selected="selected"' : ''; ?>  ><?php echo "45 dni" ?></option>
-							<option value="999"  <?php echo $shop_delivery == 999 ? 'selected="selected"' : ''; ?>  ><?php echo "Brak" ?></option>
+							
 						</select>
 						<input type="hidden" name="deliv_id_tow" value="<?php echo $id_tow; ?>" />
 					</form>
