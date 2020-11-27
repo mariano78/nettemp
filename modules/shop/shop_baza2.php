dbb3f9f97a689dd81be586daa4537fe497e5afc8
mariano78 <mariano78@interia.pl>
2020-11-27 14:17:57 +0100

    Update shop_baza2.php

--- modules/shop/shop_baza2.php
+++ modules/shop/shop_baza2.php
@@ -11,7 +11,7 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 // Dołączam ustawienia Oracle i sdk shoper
 include("$root/modules/shop/shop_settings.php");
 
-
+$cat_id = '';
 try {
    
     $categoriesResource = new DreamCommerce\ShopAppstoreLib\Resource\Category($client);
