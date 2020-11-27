984a2a53959af4082e197a3bb815f7235043161e
mariano78 <mariano78@interia.pl>
2020-11-27 22:44:35 +0100

    Update shop_dod_description_from_csv.php

--- modules/shop/shop_dod_description_from_csv.php
+++ modules/shop/shop_dod_description_from_csv.php
@@ -54,7 +54,7 @@ $time_pre = microtime(true);
 						//echo $sql."\n";
 						$clob = OCINewDescriptor($conn, OCI_D_LOB);
 						$stmt = OCIParse($conn, $sql);
-						OCIBindByName($stmt, ':lob', &$clob, -1, OCI_B_CLOB);
+						OCIBindByName($stmt, ':lob', $clob, -1, OCI_B_CLOB);
 						OCIExecute($stmt,OCI_DEFAULT);
 						if($clob->save($description_csv)){
 							OCICommit($conn);
