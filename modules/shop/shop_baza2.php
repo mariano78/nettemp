9fc42bc0be3ae3f3bd7944ee2d264e90343d6141
mariano78 <mariano78@interia.pl>
2020-11-27 14:12:20 +0100

    qw

--- modules/shop/shop_baza2.php
+++ modules/shop/shop_baza2.php
@@ -14,55 +14,39 @@ include("$root/modules/shop/shop_settings.php");
 
 try {
    
-    $resource = new DreamCommerce\ShopAppstoreLib\Resource\Category($client);
+    $categoriesResource = newDreamCommerce\ShopAppstoreLib\Resource\Category($client);
 	$currentPage = 1;
-    $result = $resource->get();
-	$pages = $result->pages;
+    $categoriesResult = $categoriesResource->get();
+	$pages = $categoriesResult->pages;
 	
 	
 	
-	while($currentPage <= $result->getPageCount() ){
+	while($currentPage <= $categoriesResult->getPageCount() ){
 		
-		$result = $resource->page($currentPage)->limit(50)->get();
+		$categoriesResult = $categoriesResource->page($currentPage)->limit(50)->get();
 		
-				$count = $result->count;
-				echo "count_".$count."\n";
+			$categories = array();
+			foreach($categoriesResult as $c){
 				
-				if ($count != '0') {
-				
-					foreach($result as $r){
-						
-						echo $r->category_id."-".$r->translations->pl_PL->name." <br>";
-						$idc = $r->category_id;
-						$nazwa = $r->translations->pl_PL->name;
-						$seo_name = pl_charset($nazwa).'.html';
-						
-						
-						$resource2 = new DreamCommerce\ShopAppstoreLib\Resource\Category($client);
+			$categories[$c->category_id] = $c->translations->pl_PL->name;
+			$cat_id = $c->category_id;
+			}
+			
+			$resource = new DreamCommerce\ShopAppstoreLib\Resource\CategoriesTree($client);
+			
+			$id = $cat_id;
+			$result = $resource->get($id);
+			
+			$renderNode = function($start, $level = 1) use (&$renderNode, $categories){
 
-							$result2 = $resource2->put($idc, array(
-								'translations' => array(
-									'pl_PL' => array(
-										'seo_url' => $seo_name
-									)
-								)
-							));
-
-							if($result2){
-								echo 'Category updated';
-							}
-						
-						
-						
-						
-						
-						
-						
-					}
-
-
-
-
+			foreach($start as $i) {
+				printf("%s #%d - %s\n", str_repeat('-', $level), $i->id, $categories[$i->id]);
+				if (!empty($i->__children)) {
+					$renderNode($i->__children, $level + 1);
+				}
+			}
+			
+		
 				$currentPage++;
 				}
 				
