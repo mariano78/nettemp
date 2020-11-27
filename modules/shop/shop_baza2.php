fc49f213c0660e434e4759a4aab83153da72a2b1
mariano78 <mariano78@interia.pl>
2020-11-27 14:55:40 +0100

    Update shop_baza2.php

--- modules/shop/shop_baza2.php
+++ modules/shop/shop_baza2.php
@@ -11,33 +11,50 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 // Dołączam ustawienia Oracle i sdk shoper
 include("$root/modules/shop/shop_settings.php");
 
-
-try{
-    
-
+$cat_id = '';
+try {
+   
     $categoriesResource = new DreamCommerce\ShopAppstoreLib\Resource\Category($client);
+	$currentPage = 1;
     $categoriesResult = $categoriesResource->get();
+	$pages = $categoriesResult->pages;
+	
+	while($currentPage <= $categoriesResult->getPageCount() ){
+		
+		$categoriesResult = $categoriesResource->page($currentPage)->limit(50)->get();
+		
+			$categories = array();
+			
+			foreach($categoriesResult as $c){
+			$cat_id = $c->category_id;
+				
+			$categories[$c->category_id] = $c->translations->pl_PL->name;
+			
+			}
+			$currentPage++;	
+	}
+			
+			//var_dump($categories);
+			
+			$resource = new DreamCommerce\ShopAppstoreLib\Resource\CategoriesTree($client);
+			
+			$id = $cat_id;
+			$result = $resource->get($id);
+			
+			$renderNode = function($start, $level = 1) use (&$renderNode, $categories){
 
-    $categories = array();
-    foreach($categoriesResult as $c){
-        $categories[$c->category_id] = $c->translations->pl_PL->name;
-    }
+			foreach($start as $i) {
+				printf("%s #%d - %s\n", str_repeat('-', $level), $i->id, $categories[$i->id]);
+				if (!empty($i->__children)) {
+					$renderNode($i->__children, $level + 1);
+				}
+			}
 
-    $resource = new DreamCommerce\ShopAppstoreLib\Resource\CategoriesTree($client);
-    $result = $resource->get();
+			};
 
-    $renderNode = function($start, $level = 1) use (&$renderNode, $categories){
-
-        foreach($start as $i) {
-            printf("%s #%d - %s\n", str_repeat('-', $level), $i->id, $categories[$i->id]);
-            if (!empty($i->__children)) {
-                $renderNode($i->__children, $level + 1);
-            }
-        }
-
-    };
-
-    $renderNode($result);
+$renderNode($result);		
+	
+   
 } catch(DreamCommerce\ShopAppstoreLib\Exception\Exception $ex) {
     die($ex->getMessage());
 }
