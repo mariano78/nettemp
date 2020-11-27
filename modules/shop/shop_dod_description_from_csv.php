15a688947645775ea84f61d6492423698517ea3f
mariano78 <mariano78@interia.pl>
2020-11-28 00:08:47 +0100

    Update shop_dod_description_from_csv.php

--- modules/shop/shop_dod_description_from_csv.php
+++ modules/shop/shop_dod_description_from_csv.php
@@ -60,8 +60,8 @@ $time_pre = microtime(true);
 						$clob = OCI_New_Descriptor($conn, OCI_D_LOB);
 						OCI_Bind_By_Name($stmt, ':lob', $clob, -1, OCI_B_CLOB);
 						OCIExecute($stmt,OCI_DEFAULT);
-						$clob->writetemporary("coś")){
-						oci_execute($stmt);							}
+						$clob->writetemporary("coś");
+						oci_execute($stmt);							
 						$clob->close();
 						//OCIFreeStatement($stmt);
 						
