1e156f1781c75e60f4e26e2353d67a5b66cb7fd7
mariano78 <mariano78@interia.pl>
2020-11-28 11:24:26 +0100

    Update shop_dod_description_from_csv.php

--- modules/shop/shop_dod_description_from_csv.php
+++ modules/shop/shop_dod_description_from_csv.php
@@ -29,7 +29,7 @@ $syncstatus = 0;
 	   $ean_csv = $getData[1];
 
 	   $description_csv = $getData[2];
-	   $description_csv = "MY big string";
+	   $description_csv = trim($description_csv);
 	   
 	   
 		$stid = oci_parse($conn, 'SELECT ID FROM JFOX_TOWAR_KARTOTEKI WHERE TO_KK_1 LIKE :eean');
