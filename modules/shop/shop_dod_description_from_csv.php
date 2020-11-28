06b3f128baa0b794768f68c53cc74250a51a69aa
mariano78 <mariano78@interia.pl>
2020-11-28 09:55:08 +0100

    Update shop_dod_description_from_csv.php

--- modules/shop/shop_dod_description_from_csv.php
+++ modules/shop/shop_dod_description_from_csv.php
@@ -18,14 +18,6 @@ $pominietych = 0;
 $akcja = 5;
 $syncstatus = 0;
 
-//$db->exec("UPDATE shop SET value='$syncstatus' WHERE option='syncstatus'");
-
-// 1. Pobieramy z bazy oracle dane o produkcie 
-// 2. Sprawdzamy czy w shoperze istnieje produkt z kodem z oracle - dodajemy lub aktualizujemy 
-$time_pre = microtime(true);
-//$stid = oci_parse($conn, 'SELECT * FROM INFOR_SHOPER_EXP');
-//oci_execute($stid);
-
 
     
 		$filename = "opisy1.csv";
@@ -42,13 +34,12 @@ $time_pre = microtime(true);
 	 
 	   
         }
+		}
       
         fclose($file);  
 		oci_free_statement($stid);
 		oci_close($conn);
      
-		
-
 
 ?>
 
