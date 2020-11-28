e5d17bb7915414c21df16c9785f20b510b0ef2bd
mariano78 <mariano78@interia.pl>
2020-11-28 14:58:36 +0100

    Update shop_baza.php

--- modules/shop/shop_baza.php
+++ modules/shop/shop_baza.php
@@ -38,7 +38,7 @@ $ffkodok = isset($_POST['ffkodok']) ? $_POST['ffkodok'] : '';
 
 if (!empty($ffkodok)){
     
-	//$db = new PDO("sqlite:$root/dbf/nettemp.db");
+	$db = new PDO('sqlite:$root/dbf/nettemp.db');
     $db->exec("UPDATE shop SET value = '$ffkod' WHERE option = 'fkod'") or die ($db->lastErrorMsg());
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
