fbcddca0267e39d79348ac0008154183314fe462
mariano78 <mariano78@interia.pl>
2020-11-28 11:14:21 +0100

    Update shop_dod_description_from_csv.php

--- modules/shop/shop_dod_description_from_csv.php
+++ modules/shop/shop_dod_description_from_csv.php
@@ -27,13 +27,35 @@ $syncstatus = 0;
            {  
 	   $tokod_csv = $getData[0];
 	   $ean_csv = $getData[1];
+
 	   $description_csv = $getData[2];
 	   $description_csv = trim($description_csv);
-	   echo $description_csv;
-	   
 	 
+	   $id_tow = $row['ID']; //kod towaru w RB
+					echo $id_tow;
+					// etc.
+
+						
+						$sql = "UPDATE SHOPPER_PRODUCTS SET SHOP_TO_DESCRIPTION = EMPTY_CLOB() WHERE ID_TOW = '$id_tow' RETURNING SHOP_TO_DESCRIPTION INTO :lob";
+						echo $sql."\n";
+						
+						$stmt = OCI_Parse($conn, $sql);
+						$clob = OCI_New_Descriptor($conn, OCI_D_LOB);
+						OCI_Bind_By_Name($stmt, ':lob', $clob, -1, OCI_B_CLOB);
+						OCI_Execute($stmt,OCI_DEFAULT);
+						$clob->save($description_csv);
+						
+						
+						
+						OCIFreeStatement($stmt);
 	   
-        }
+	   
+	   
+	   
+	   
+	   
+	   
+			}
 		}
       
         fclose($file);  
