e7657b7dda5528ea81596aa1f86a7d3fedac1848
mariano78 <mariano78@interia.pl>
2020-11-30 18:44:08 +0100

    Update shop_parse_products.php

--- modules/shop/shop_parse_products.php
+++ modules/shop/shop_parse_products.php
@@ -32,9 +32,10 @@ include("$root/modules/shop/shop_settings.php");
 			while (($row2 = oci_fetch_array($stid2, OCI_ASSOC)) != false) {
 				
 				$tow_status = $row2['TO_STATUS_HANDL']; //status towaru w jfox
-				echo $tow_status;
 				
-				if($tow_status != 'spr' && $tow_status && 'spz' && $tow_status != 'wyp'){
+				
+				if($tow_status != "spr" && $tow_status && "spz" && $tow_status != "wyp"){
+					echo "TOW STAT-".$tow_status;
 					
 					//update shop_prod
 					$stid3 = oci_parse($conn, 'UPDATE SHOPPER_PRODUCTS SET IS_DELETED = :is_del, IN_SHOP = :in_shop WHERE ID_TOW = :idtow4');
