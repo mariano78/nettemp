d8b96e9ce918af6e9b48529b4e3865cc12cb6623
mariano78 <mariano78@interia.pl>
2020-11-30 19:21:08 +0100

    Update shop_parse_products.php

--- modules/shop/shop_parse_products.php
+++ modules/shop/shop_parse_products.php
@@ -32,24 +32,19 @@ include("$root/modules/shop/shop_settings.php");
 			while (($row2 = oci_fetch_array($stid2, OCI_ASSOC)) != false) {
 				
 				$tow_status = $row2['TO_STATUS_HANDL']; //status towaru w jfox
-				
-				
-				if($tow_status != 'spr' && $tow_status != 'spz' && $tow_status != 'wyp'){
-					echo "TOW STAT-".$tow_status;
+				echo "TOW STAT-".$tow_status;
 					echo "\nTOW ID-".$id_tow;
+				
+				if($tow_status != 'spr' OR $tow_status != 'spz' OR $tow_status != 'wyp'){
 					
 					//update shop_prod
-					
+					$stid3 = oci_parse($conn, 'UPDATE SHOPPER_PRODUCTS SET IS_DELETED = :is_del, IN_SHOP = :in_shop WHERE ID_TOW = :idtow4');
 					$is_del = 'Y';
 					$in_shop = 'N';
-					$stid3 = oci_parse($conn, "UPDATE SHOPPER_PRODUCTS SET IS_DELETED ='".$is_del."', IN_SHOP ='".$is_del."' WHERE ID_TOW = :idtow4");
-					
 					oci_bind_by_name($stid3, ":idtow4", $id_tow);
-					//oci_bind_by_name($stid3, ":is_del", $is_del);
-					//oci_bind_by_name($stid3, ":in_shop", $in_shop);
+					oci_bind_by_name($stid3, ":is_del", $is_del);
+					oci_bind_by_name($stid3, ":in_shop", $in_shop);
 					oci_execute($stid3);
-					//oci_commit($conn);
-					echo oci_error();
 					echo "Ustawiam \n";
 					
 				}
