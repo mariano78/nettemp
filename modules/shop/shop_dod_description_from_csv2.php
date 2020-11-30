c9ed67ce5eedf72148f9ed35719706a6124dda48
mariano78 <mariano78@interia.pl>
2020-11-30 14:11:47 +0100

    Update shop_dod_description_from_csv2.php

--- modules/shop/shop_dod_description_from_csv2.php
+++ modules/shop/shop_dod_description_from_csv2.php
@@ -18,7 +18,7 @@ $pominietych = 0;
 $akcja = 5;
 $syncstatus = 0;
 
-		$filename = "opisy.csv";
+		$filename = "bill.csv";
         $file = fopen($filename, "r");
 		if ($file) {
           while (($getData = fgetcsv($file, 10000, ";")) !== FALSE)
