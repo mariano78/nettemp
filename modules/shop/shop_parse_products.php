ea07638ffb159dfce3bee215bec5b80682021a71
mariano78 <mariano78@interia.pl>
2020-11-30 19:54:49 +0100

    Update shop_parse_products.php

--- modules/shop/shop_parse_products.php
+++ modules/shop/shop_parse_products.php
@@ -24,9 +24,9 @@ include("$root/modules/shop/shop_settings.php");
 			//JFOX_RB_TOWAR_KARTOTEKI.TO_STATUS_HANDL
 			
 			$stid2 = oci_parse($conn, 'SELECT * FROM JFOX_RB_TOWAR_KARTOTEKI WHERE ID_TOW = :idtow3 AND IS_DELETED = :isdel');
-			oci_bind_by_name($stid2, ":idtow3", $id_tow);
+			oci_bind_by_name($stid2, ':idtow3', $id_tow);
 			$isdel = 'N';
-			oci_bind_by_name($stid2, ":isdel", $isdel);
+			oci_bind_by_name($stid2, ':isdel', $isdel);
 			oci_execute($stid2);
 			
 			while (($row2 = oci_fetch_array($stid2, OCI_ASSOC)) != false) {
