a74cb652a61b82d972faba71d13491bb19caad9b
mariano78 <mariano78@interia.pl>
2020-12-10 15:30:08 +0100

    Update shop_baza_test.php

--- modules/shop/shop_baza_test.php
+++ modules/shop/shop_baza_test.php
@@ -659,7 +659,7 @@ $stid2 = oci_parse($conn, "$sql");
 				<td class="col-md-0">
 					
 					<form action="" method="post"  class="form-inline">
-						<select name="dostawa" class="form-control input-sm small" onchange="this.form.submit()" style="width: 130px;" >
+						<select name="dostawa" class="form-control input-sm small" onchange="this.form.submit()" style="width: 100px;" >
 							<option class="label-danger" value="999"  <?php echo $shop_delivery == 999 ? 'selected="selected"' : ''; ?>  ><?php echo "Brak" ?></option>
 							<option value="2"  <?php echo $shop_delivery == 2 ? 'selected="selected"' : ''; ?>  ><?php echo "2 dni" ?></option>
 							<option value="6"  <?php echo $shop_delivery == 6 ? 'selected="selected"' : ''; ?>  ><?php echo "10 dni" ?></option>
@@ -673,7 +673,7 @@ $stid2 = oci_parse($conn, "$sql");
 				<td class="col-md-1">
 					
 					<form action="" method="post"  class="form-inline">
-						<select name="dostawa_typ" class="form-control input-sm small" onchange="this.form.submit()" style="width: 130px;" >
+						<select name="dostawa_typ" class="form-control input-sm small" onchange="this.form.submit()" style="width: 100px;" >
 							<option class="label-danger" value="999"  <?php echo $shop_delivery_typ == 999 ? 'selected="selected"' : ''; ?>  ><?php echo "Brak" ?></option>
 							<option value="2"  <?php echo $shop_delivery_typ == 2 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 20" ?></option>
 							<option value="3"  <?php echo $shop_delivery_typ == 3 ? 'selected="selected"' : ''; ?>  ><?php echo "DPD - 40" ?></option>
