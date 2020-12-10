5ae13ff55b21175fb70b3656561306aaf03ff8c9
mariano78 <mariano78@interia.pl>
2020-12-10 15:50:26 +0100

    Update update_db_new.php

--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -331,6 +331,7 @@ $updates['2020-11-20 11:49:40'][]="INSERT INTO shop ('option', 'value') VALUES (
 $updates['2020-11-20 11:59:40'][]="INSERT INTO shop ('option', 'value') VALUES ('fkod','%')";
 $updates['2020-11-20 11:59:45'][]="INSERT INTO shop ('option', 'value') VALUES ('fgrupa','%')";
 $updates['2020-11-20 11:59:47'][]="INSERT INTO shop ('option', 'value') VALUES ('fsklep','%')";
+$updates['2020-11-20 11:59:49'][]="INSERT INTO shop ('option', 'value') VALUES ('fstat','%')";
 
 
 
