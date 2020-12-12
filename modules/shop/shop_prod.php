23b130132c6fd8bba93c104a59681e6e7dd6ba6a
Mariusz Musik <m@serwer-robelit.local>
2020-12-12 14:13:01 +0100

    Update shop_prod.php

--- modules/shop/shop_prod.php
+++ modules/shop/shop_prod.php
@@ -166,7 +166,7 @@ if ($to_grupa == 'IZOLK' OR $to_grupa == 'PLSRU' OR $to_grupa == 'IZOLM' OR $to_
 		}
 	
 	}
-	$to_related_inshop2 = implode(",",$to_related_in_shop);
+	$to_related_inshop2 = implode(",",$to_related_inshop);
 	echo $to_related_inshop2;
 	//var_dump($to_related_inshop); 
 
