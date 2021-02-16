59c2ad05c10e686c6da6c1133872d615490103e6
mariano78 <mariano78@interia.pl>
2021-02-16 13:45:38 +0100

    Update shop_zam.php

--- modules/shop/shop_zam.php
+++ modules/shop/shop_zam.php
@@ -11,12 +11,34 @@ if(!empty($_SERVER["DOCUMENT_ROOT"])){
 // Dołączam ustawienia Oracle i sdk shoper
 include("$root/modules/shop/shop_settings.php");
 
-			$resource = new DreamCommerce\ShopAppstoreLib\Resource\Order($client);
-    $result = $resource->get();
-
-    foreach($result as $r){
+	$resource = new DreamCommerce\ShopAppstoreLib\Resource\Order($client);
+    $currentPage = 1;
+	$currentProd = 1;;
+	$result = $resource->get();
+	$pages = $result->pages;
+	
+	
+	while($currentPage <= $result->getPageCount() ){
+	  
+		$result = $resource->page($currentPage)->limit(50)->get();
+	
+				//var_dump($result);
+				$count = $result->count;
+				echo "count_".$count."\n";
+				
+				foreach($result as $r){
         printf("#%d - %.2f (@%s)\n", $r->order_id, $r->sum, $r->date);
     }
+		
+		$currentPage++;		
+	}
+	
+	
+
+    
  
 
 ?>
+
+
+	
