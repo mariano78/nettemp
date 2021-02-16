86f72970f8308cb8001edcd36a67db1d1d954837
mariano78 <mariano78@interia.pl>
2021-02-16 15:42:00 +0100

    Update shop_zam.php

--- modules/shop/shop_zam.php
+++ modules/shop/shop_zam.php
@@ -38,7 +38,7 @@ include("$root/modules/shop/shop_settings.php");
 					echo $kwota_netto;
 					$global_kwota_netto += $kwota_netto;
 					
-					echo $global_kwota_netto. "\n";
+					echo "\n".$global_kwota_netto."\n";
 					
 				
 					
