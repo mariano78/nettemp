abf2d6f252636a529a5f8afb4b975070a72b4086
mariano78 <mariano78@interia.pl>
2021-02-17 19:03:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -121,6 +121,29 @@ function new_seen($seen){
 ?>
 
 
+<li <?php echo $id == 'status' ? ' class="active"' : ''; ?>><a href="index.php?id=status"><span class="glyphicon glyphicon-th-large" aria-hidden="true"> Status</span></a></li>
+<li <?php echo $id == 'view' ? ' class="active"' : ''; ?>><a href="index.php?id=view&type=temp&max=<?php echo $nts_charts_max?>"><span class="glyphicon glyphicon-stats" aria-hidden="true"> Charts</span></a></li>
+<?php
+	if($nts_screen=='on')
+	{
+	?>
+	<li <?php echo $id == 'screen' ? ' class="active"' : ''; ?>><a href="index.php?id=screen"><span class="glyphicon glyphicon-modal-window" aria-hidden="true"> Screen</span></a></li>
+<?php
+	}
+	if(($_SESSION["perms"] == 'adm') || (isset($_SESSION["user"]))) {
+   if (( $numsimple >= "1") || ( $numsimple2 >= "1"))  {
+   ?>
+   <li <?php echo $id == 'controls' ? ' class="active"' : ''; ?>><a href="index.php?id=controls"><span class="glyphicon glyphicon-record" aria-hidden="true"> Controls</span></a></li>
+<?php
+	}
+ 	if($_SESSION["perms"] == 'adm') {
+	
+	if ($nts_mapon == 'on'){
+	?>
+
+
+
+
 
 <li <?php echo $id == 'tools' ? ' class="active"' : ''; ?>><a href="index.php?id=tools"><span class="glyphicon glyphicon-wrench" aria-hidden="true"> Tools</span></a></li>
 <?php
