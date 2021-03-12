22d4f7129b6522da7331ca38903aad7df7ea46fb
mariano78 <mariano78@interia.pl>
2021-03-12 12:13:13 +0100

    Update shop_img.php

--- modules/shop/shop_img.php
+++ modules/shop/shop_img.php
@@ -30,7 +30,7 @@ $resource = new DreamCommerce\ShopAppstoreLib\Resource\Product($client);
 	$resource->filters([
 						
 						'translations.pl_PL.active' =>true,
-						'code' =>'0001399'
+						'stock.code' =>['LIKE'] => $code_to_update
 						
 						]);
 	}
