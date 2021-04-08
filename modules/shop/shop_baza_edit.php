8c504cca5a708e406a6fd5ce8ea5bda42aae1be5
mariano78 <mariano78@interia.pl>
2021-04-08 12:21:36 +0200

    Update shop_baza_edit.php

--- modules/shop/shop_baza_edit.php
+++ modules/shop/shop_baza_edit.php
@@ -5,8 +5,8 @@
       tinymce.init({
         selector: '#mytextarea',
 		language: 'pl',
-		plugins: 'autoresize link code image table wordcount', 
-		toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link | code | image | table',
+		plugins: 'autoresize link code image table wordcount lists', 
+		toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | bullist | numlist | link | code | image | table',
 		content_css: '/html/shop.css'
 		
       });
