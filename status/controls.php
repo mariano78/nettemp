f4b7a22f7d0ad39964c97cb06fd53d60b02d18f6
mariano78 <mariano78@interia.pl>
2021-04-26 10:33:45 +0200

    Update controls.php

--- status/controls.php
+++ status/controls.php
@@ -100,7 +100,12 @@ function gpio_curl_onoff($ip,$gpio,$rom,$action,$moment_time){
 		
 	$ch = curl_init();
 	$optArray = array(
-		CURLOPT_URL => "$ip/control?cmd=$method,$gpio,$set,$moment_time",
+	
+		if ($action=='moment'){
+			CURLOPT_URL => "$ip/control?cmd=$method,$gpio,$set,$moment_time",
+		}else{
+			CURLOPT_URL => "$ip/control?cmd=$method,$gpio,$set,$moment_time",
+		}
 		CURLOPT_HEADER => true,
 		CURLOPT_NOBODY => true,
 		CURLOPT_RETURNTRANSFER => true,
