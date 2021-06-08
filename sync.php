4587d4e73a337757d910c1e5dbd3e9effbc7aa02
mariano78 <mariano78@interia.pl>
2021-06-08 12:43:36 +0200

    Create sync.php

---
+++ sync.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+// name:
+// type: temp, humid, relay, lux, press, humid, gas, water, elec, volt, amps, watt, trigger
+// device: ip, wireless, remote, gpio, i2c, usb
+
+// definied source (middle part): tty, ip, gpio number
+
+// curl --connect-timeout 3 -G "http://172.18.10.10/receiver.php" -d "value=1&key=123456&device=wireless&type=gas&ip=172.18.10.9"
+// curl --connect-timeout 3 -G "http://172.18.10.10/receiver.php" -d "value=20&key=123456&device=wireless&type=elec&ip=172.18.10.9"
+// php-cgi -f receiver.php key=123456 rom=new_12_temp value=23
+
+if (isset($_GET['code'])) { 
+    $key = $_GET['code'];
+} else { 
+    $key='';
+}
+
+php-cgi -f /modules/shop/shop_prod.php key=123456 rom=new_12_temp value=23
+
+?>
+
