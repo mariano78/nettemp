41c05f8e691341e2f2c9ad9d7d26ce25b2c0ea25
mariano78 <mariano78@interia.pl>
2021-06-14 15:25:24 +0200

    Update db.sh

--- install/db/db.sh
+++ install/db/db.sh
@@ -7,13 +7,14 @@ echo $ROOTPASSWDDB >> /root/mysql_pass
 MAINDB=nettemp
 DBUSER=nettempusr
 echo $dir
+
 sed -i "s/db_pass_mysql/$PASSWDDB/g" $dir/config/config.php
 
 # If /root/mysql_pass exists then it won't ask for root password
 if [ -f /root/mysql_pass ]; then
 
     mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER mario@localhost IDENTIFIED BY 'Ala1Ala2';"
+    mysql -e "CREATE USER '$DBUSER'@localhost IDENTIFIED BY '$PASSWDDB';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
     mysql -e "FLUSH PRIVILEGES;"
 
