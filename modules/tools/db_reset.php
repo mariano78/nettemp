bb88000fb1ff770d02ab69fc8bb19ddd814be37e
mariano78 <mariano78@interia.pl>
2021-06-14 14:34:15 +0200

    Update db_reset.php

--- modules/tools/db_reset.php
+++ modules/tools/db_reset.php
@@ -8,40 +8,26 @@ shell_exec("sudo rm -rf $ROOT/tmp");
 shell_exec("sudo mkdir $ROOT/tmp");
 
 include("$ROOT/config/config.php");
-
-
-
-// Name of the file
+ 
+$conn = new mysqli($host, $user, $password, $db);
 $filename = '$ROOT/modules/tools/nettemp.sql';
-
-// Connect to MySQL server
-mysql_connect($host, $user, $password) or die('Error connecting to MySQL server: ' . mysql_error());
-// Select database
-mysql_select_db($db) or die('Error selecting MySQL database: ' . mysql_error());
-
-// Temporary variable, used to store current query
-$templine = '';
-// Read in entire file
+$op_data = '';
 $lines = file($filename);
-// Loop through each line
 foreach ($lines as $line)
 {
-// Skip it if it's a comment
-if (substr($line, 0, 2) == '--' || $line == '')
-    continue;
-
-// Add this line to the current segment
-$templine .= $line;
-// If it has a semicolon at the end, it's the end of the query
-if (substr(trim($line), -1, 1) == ';')
-{
-    // Perform the query
-    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
-    // Reset temp variable to empty
-    $templine = '';
+    if (substr($line, 0, 2) == '--' || $line == '')//This IF Remove Comment Inside SQL FILE
+    {
+        continue;
+    }
+    $op_data .= $line;
+    if (substr(trim($line), -1, 1) == ';')//Breack Line Upto ';' NEW QUERY
+    {
+        $conn->query($op_data);
+        $op_data = '';
+    }
 }
-}
- echo "Tables imported successfully";
+echo "Table Created Inside " . $database . " Database.......";
+ 
 
 
 
