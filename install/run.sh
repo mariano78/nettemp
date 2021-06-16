3cc7984d2168aeb99fe93b378f94384465578744
mariano78 <mariano78@interia.pl>
2021-06-16 14:56:12 +0200

    Update run.sh

--- install/run.sh
+++ install/run.sh
@@ -4,9 +4,9 @@
 
 mkdir -p $dir/tmp
 mkdir -p $dir/config
-sudo chown -R root.www-data $dir
-sudo chmod -R 775 $dir
-sudo chmod -R g+s $dir
+#sudo chown -R root.www-data $dir
+#sudo chmod -R 775 $dir
+#sudo chmod -R g+s $dir
 rm -f $dir/config/config.php
 cp $dir/install/config/config.php $dir/config/config.php
 
